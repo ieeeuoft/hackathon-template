40ad1356e0443d89f9681322a463616c3f750cf3
lisa-sa-li <lisasiaili@gmail.com>
2020-05-02 22:02:23 -0400

    created file directory

--- hackathon_site/dashboard/frontend/src/pages/dashboard/App.js
+++ hackathon_site/dashboard/frontend/src/pages/dashboard/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './../../logo.svg';
+import './App.scss';
 
 function App() {
   return (
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          IEEeeeeee
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
