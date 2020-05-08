2637cab8fa899acaba03e27dad7996eac14c94a7
lisa-sa-li <lisasiaili@gmail.com>
2020-05-07 21:07:02 -0400

    deleted commented code

--- hackathon_site/dashboard/frontend/src/App.test.js
+++ hackathon_site/dashboard/frontend/src/App.test.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test('renders placeholder text', () => {
   const { getByText } = render(<App />);
   const textElement = getByText(/IEEeeeeee/i);
