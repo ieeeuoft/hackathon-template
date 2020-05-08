863cfbe12a2831058aa97ec1f1167523668cda01
lisa-sa-li <lisasiaili@gmail.com>
2020-05-07 20:22:11 -0400

    made spaces more consistent

--- hackathon_site/dashboard/frontend/src/App.test.js
+++ hackathon_site/dashboard/frontend/src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders placeholder text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/IEEeeeeee/i);
+  expect(textElement).toBeInTheDocument();
 });
