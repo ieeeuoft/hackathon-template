70d4ec85532cf7e76e2bd6554c0cce0d8700fa5a
Graham Hoyes <accounts@grahamhoyes.com>
2020-05-10 13:19:15 -0400

    Format things with prettier

--- hackathon_site/dashboard/frontend/src/App.js
+++ hackathon_site/dashboard/frontend/src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          IEEeeeeee
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>IEEeeeeee</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
