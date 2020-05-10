240723d93f7553cc2bdc28a20f93825588582ec4
lisa-sa-li <lisasiaili@gmail.com>
2020-05-10 19:15:43 -0400

    applied prettier

--- hackathon_site/dashboard/frontend/src/App.js
+++ hackathon_site/dashboard/frontend/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.scss';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import NavBar from './components/general/Navbar/Navbar';
+import React from "react";
+import "./App.scss";
+import { HashRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/general/Navbar/Navbar";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				< NavBar/>
-			</Router>
-			<div className="App-header">
-				<p>IEEeeeeeee</p>
-			</div>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Router>
+                <NavBar />
+            </Router>
+            <div className="App-header">
+                <p>IEEeeeeeee</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
+++ hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
@@ -1,70 +1,70 @@
-import React, { PureComponent } from 'react'
-import styles from './Navbar.module.scss';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from './../../../logo.svg';
-import { Home, ShoppingCart, Notifications, ListAlt, AccountCircle, InsertChartOutlined } from '@material-ui/icons';
-import { ReactComponent as Inventory } from './../../../assets/images/icons/Hardware.svg';
-import { IconButton, AppBar, Toolbar, Typography } from '@material-ui/core';
+import React, { PureComponent } from "react";
+import styles from "./Navbar.module.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "./../../../logo.svg";
+import {
+    Home,
+    ShoppingCart,
+    Notifications,
+    ListAlt,
+    AccountCircle,
+    InsertChartOutlined,
+} from "@material-ui/icons";
+import { ReactComponent as Inventory } from "./../../../assets/images/icons/Hardware.svg";
+import { IconButton, AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export default class Navbar extends PureComponent {
-	render() {
-		return (
-			<AppBar className={styles.nav}>
-          		<Toolbar className={styles.navToolbar}>
-					<div className={styles.navLogo}>
-						<Logo className={styles.navLogoImg}/>
-            			<Typography variant="h6">Hackathon Name</Typography>
-					</div>		
+    render() {
+        return (
+            <AppBar className={styles.nav}>
+                <Toolbar className={styles.navToolbar}>
+                    <div className={styles.navLogo}>
+                        <Logo className={styles.navLogoImg} />
+                        <Typography variant="h6">Hackathon Name</Typography>
+                    </div>
 
-					<div className={styles.navIcons}>
-						<Link to={'/orders'}>
-							<IconButton color="inherit" aria-label="Orders">
-								<ListAlt/>
-								<span>Orders</span>
-							</IconButton>
-						</Link>
+                    <div className={styles.navIcons}>
+                        <Link to={"/orders"}>
+                            <IconButton color="inherit" aria-label="Orders">
+                                <ListAlt />
+                                <span>Orders</span>
+                            </IconButton>
+                        </Link>
 
-						<Link to={'/teams'}>
-							<IconButton color="inherit" aria-label="Teams">
-								<AccountCircle/> Teams
-							</IconButton>
-						</Link>
+                        <Link to={"/teams"}>
+                            <IconButton color="inherit" aria-label="Teams">
+                                <AccountCircle /> Teams
+                            </IconButton>
+                        </Link>
 
-						<Link to={'/reports'}>
-							<IconButton color="inherit" aria-label="Reports">
-								<InsertChartOutlined/> Reports
-							</IconButton>
-						</Link>
+                        <Link to={"/reports"}>
+                            <IconButton color="inherit" aria-label="Reports">
+                                <InsertChartOutlined /> Reports
+                            </IconButton>
+                        </Link>
 
-						<Link to={'/inventory'}>
-							<IconButton color="inherit" aria-label="Inventory">
-								<Inventory /> Inventory
-							</IconButton>
-						</Link>
+                        <Link to={"/inventory"}>
+                            <IconButton color="inherit" aria-label="Inventory">
+                                <Inventory /> Inventory
+                            </IconButton>
+                        </Link>
 
-						<Link to={'/cart'}>
-							<IconButton color="inherit" aria-label="Cart">
-								<ShoppingCart /> Cart
-							</IconButton>
-						</Link>
+                        <Link to={"/cart"}>
+                            <IconButton color="inherit" aria-label="Cart">
+                                <ShoppingCart /> Cart
+                            </IconButton>
+                        </Link>
 
-						<IconButton color="inherit" aria-label="Notifications">
-							<Notifications /> Notifications
-						</IconButton>
+                        <IconButton color="inherit" aria-label="Notifications">
+                            <Notifications /> Notifications
+                        </IconButton>
 
-						<IconButton color="inherit" aria-label="Logout">
-							Logout
-						</IconButton>
-
-						
-						
-					</div>
-          		</Toolbar>
-			</AppBar>
-
-			// <header className={styles.header}>
-			
-			// </header>
-        )
+                        <IconButton color="inherit" aria-label="Logout">
+                            Logout
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        );
     }
 }
