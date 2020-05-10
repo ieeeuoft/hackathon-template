eb86c2280d9e9c5303af554c18dfc8a42820c643
lisa-sa-li <lisasiaili@gmail.com>
2020-05-10 19:27:20 -0400

    fixed graham's comments

--- hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
+++ hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import styles from "./Navbar.module.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "./../../../logo.svg";
@@ -13,58 +13,61 @@ import {
 import { ReactComponent as Inventory } from "./../../../assets/images/icons/Hardware.svg";
 import { IconButton, AppBar, Toolbar, Typography } from "@material-ui/core";
 
-export default class Navbar extends PureComponent {
-    render() {
-        return (
-            <AppBar className={styles.nav}>
-                <Toolbar className={styles.navToolbar}>
-                    <div className={styles.navLogo}>
-                        <Logo className={styles.navLogoImg} />
-                        <Typography variant="h6">Hackathon Name</Typography>
-                    </div>
+const Navbar = () => (
+    <AppBar className={styles.nav}>
+        <Toolbar className={styles.navToolbar}>
+            <div className={styles.navLogo}>
+                <Logo className={styles.navLogoImg} />
+                <Typography variant="h6">Hackathon Name</Typography>
+            </div>
 
-                    <div className={styles.navIcons}>
-                        <Link to={"/orders"}>
-                            <IconButton color="inherit" aria-label="Orders">
-                                <ListAlt />
-                                <span>Orders</span>
-                            </IconButton>
-                        </Link>
+            <div className={styles.navIcons}>
+                <Link to={"/"}>
+                    <IconButton color="inherit" aria-label="Dashboard">
+                        <Home /> Dashboard
+                    </IconButton>
+                </Link>
 
-                        <Link to={"/teams"}>
-                            <IconButton color="inherit" aria-label="Teams">
-                                <AccountCircle /> Teams
-                            </IconButton>
-                        </Link>
+                <Link to={"/orders"}>
+                    <IconButton color="inherit" aria-label="Orders">
+                        <ListAlt /> Orders
+                    </IconButton>
+                </Link>
 
-                        <Link to={"/reports"}>
-                            <IconButton color="inherit" aria-label="Reports">
-                                <InsertChartOutlined /> Reports
-                            </IconButton>
-                        </Link>
+                <Link to={"/teams"}>
+                    <IconButton color="inherit" aria-label="Teams">
+                        <AccountCircle /> Teams
+                    </IconButton>
+                </Link>
 
-                        <Link to={"/inventory"}>
-                            <IconButton color="inherit" aria-label="Inventory">
-                                <Inventory /> Inventory
-                            </IconButton>
-                        </Link>
+                <Link to={"/reports"}>
+                    <IconButton color="inherit" aria-label="Reports">
+                        <InsertChartOutlined /> Reports
+                    </IconButton>
+                </Link>
 
-                        <Link to={"/cart"}>
-                            <IconButton color="inherit" aria-label="Cart">
-                                <ShoppingCart /> Cart
-                            </IconButton>
-                        </Link>
+                <Link to={"/inventory"}>
+                    <IconButton color="inherit" aria-label="Inventory">
+                        <Inventory /> Inventory
+                    </IconButton>
+                </Link>
 
-                        <IconButton color="inherit" aria-label="Notifications">
-                            <Notifications /> Notifications
-                        </IconButton>
+                <Link to={"/cart"}>
+                    <IconButton color="inherit" aria-label="Cart">
+                        <ShoppingCart /> Cart
+                    </IconButton>
+                </Link>
 
-                        <IconButton color="inherit" aria-label="Logout">
-                            Logout
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        );
-    }
-}
+                <IconButton color="inherit" aria-label="Notifications">
+                    <Notifications /> Notifications
+                </IconButton>
+
+                <IconButton color="inherit" aria-label="Logout">
+                    Logout
+                </IconButton>
+            </div>
+        </Toolbar>
+    </AppBar>
+);
+
+export default Navbar;
