8d761300c92c28f44bf2b11e749f9de9b1c294a9
lisa-sa-li <lisasiaili@gmail.com>
2020-05-10 17:38:13 -0400

    resolving merge conflict

--- hackathon_site/dashboard/frontend/src/App.js
+++ hackathon_site/dashboard/frontend/src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/general/Navbar/Navbar';
 
 function App() {
-return (
-	<div className="App">
-		<Router>
-			< NavBar/>
+	return (
+		<div className="App">
+			<Router>
+				< NavBar/>
+			</Router>
 			<div className="App-header">
 				<p>IEEeeeeee</p>
 			</div>
-		</Router>
-		
-		
-	</div>
-);
+		</div>
+	);
 }
 
 export default App;
