85a0c5a930b1281d2beef3b6668e0f46a64e2fe6
Akinori Kimura <1999akinori@gmail.com>
2020-05-15 15:07:28 -0400

    Modified dash card based on feedback and added files I forgot to add

--- hackathon_site/dashboard/frontend/src/App.js
+++ hackathon_site/dashboard/frontend/src/App.js
@@ -6,24 +6,6 @@ import "App.scss";
 import DashCard from "components/dashboard/DashCard/DashCard.js";
 
 function App() {
-    const test = [
-        {
-          name: "chair",
-          url: 5,
-          icon: 45.99
-        },
-        {
-          name: "table",
-          url: 10,
-          icon: 123.75
-        },
-        {
-          name: "sofa",
-          url: 2,
-          icon: 399.50
-        }
-      ];
-
     return (
         <div className="App">
             <StylesProvider injectFirst>
@@ -32,7 +14,6 @@ function App() {
                 </Router>
                 <div className="App-header">
                     <p>IEEeeeeeee</p>
-                    <DashCard title="name" list={test}/>
                 </div>
             </StylesProvider>
         </div>
--- hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.js
+++ hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.js
@@ -3,41 +3,30 @@ import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import styles from "./DashCard.module.scss";
-import Link from '@material-ui/core/Link';
+import Link from "@material-ui/core/Link";
 
-const ItemIcon = (url, icon) => {
-    return{
-
-    }
-}
-
-const CardItem = (name, url, icon) => {
+const CardItem = ({ name, icon }) => {
     return (
-        <Container>
+        <Container className={styles.items}>
             <Typography variant="body2">{name}</Typography>
-            <ItemIcon url={url} icon={icon} />
+            {icon}
         </Container>
     );
-}
+};
 
-const preventDefault = (event) => event.preventDefault();
-
-const DashCard = (title, {list) => {
-    const createList = list;
-    const cardItem = list.map((listItem)=>
-        <Link href={listItem.url} onClick={preventDefault}>
-            <CardItem name={listItem.name} icon={listItem.icon} />
-        </Link>
-    );
-
+const DashCard = ({ title, content }) => {
     return (
         <Container className={styles.DashCard} maxWidth={false} disableGutters={true}>
             <h2>{title}</h2>
             <Paper elevation={3} className={styles.paper} square={true}>
-                {cardItem}
+                {content.map((listItem) => (
+                    <Link href={listItem.url} rel="noopener">
+                        <CardItem name={listItem.name} icon={listItem.icon} />
+                    </Link>
+                ))}
             </Paper>
         </Container>
     );
-}
+};
 
 export default DashCard;
