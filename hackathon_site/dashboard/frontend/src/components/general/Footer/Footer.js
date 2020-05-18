8feecf282f2a1895963bf8cdf9ab9b2ff35698aa
Brenny <brennychao@gmail.com>
2020-05-18 08:33:03 -0400

    removed height and padding top from the footer.scss files, added the changable date for the footer.js added the testing for the snapshot and time

--- hackathon_site/dashboard/frontend/src/components/general/Footer/Footer.js
+++ hackathon_site/dashboard/frontend/src/components/general/Footer/Footer.js
@@ -3,16 +3,31 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import styles from "./Footer.module.scss";
 
-const Footer = () => (
-    <Container maxWidth={false} disableGutters={true}>
-        <footer className={styles.footer}>
-            <Typography variant="caption" display="block" gutterBottom>
-                Hardware Signout
-            </Typography>
-            <Typography variant="caption" display="block" gutterBottom>
-                © 2020 - build by the web team at MakeUofT (University of Toronto)
-            </Typography>
-        </footer>
-    </Container>
-);
+const Footer = () => {
+    let date = new Date();
+
+    if (date.getFullYear() === 2020) {
+        var outputMessage =
+            "© 2020 - built by the web team at MakeUofT (University of Toronto)";
+    } else {
+        var outputMessage =
+            "© 2020 - " +
+            date.getFullYear() +
+            " built by the web team at MakeUofT (University of Toronto)";
+    }
+
+    return (
+        <Container maxWidth={false} disableGutters={true}>
+            <footer className={styles.footer}>
+                <Typography variant="caption" display="block" gutterBottom>
+                    Hardware Signout
+                </Typography>
+                <Typography variant="caption" display="block" gutterBottom>
+                    {outputMessage}
+                </Typography>
+            </footer>
+        </Container>
+    );
+};
+
 export default Footer;
--- hackathon_site/dashboard/frontend/src/components/general/Footer/Footer.test.js
+++ hackathon_site/dashboard/frontend/src/components/general/Footer/Footer.test.js
@@ -4,5 +4,35 @@ import "@testing-library/jest-dom/extend-expect";
 import Footer from "./Footer";
 
 describe("Footer", () => {
-    test("renders", () => {});
+    test("renders", () => {
+        const { asFragment } = render(<Footer />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test("date", () => {
+        const fakeDate = new Date("2021-05-17");
+        const fakeYear = "2021";
+
+        const fakeDateClass = class extends Date {
+            constructor() {
+                super(fakeDate);
+            }
+
+            static now() {
+                return null;
+            }
+
+            getFakeYear() {
+                return fakeYear;
+            }
+        };
+
+        global.Date = fakeDateClass;
+        const { getByText } = render(<Footer />);
+        const footerText = getByText(/built/, { exact: false });
+
+        expect(footerText).toHaveTextContent(
+            `© 2020 - ${fakeYear} built by the web team at MakeUofT (University of Toronto)`
+        );
+    });
 });
