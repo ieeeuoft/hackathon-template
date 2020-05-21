38f49799bb6d1d28698a796d59faa21103eac146
Akinori Kimura <1999akinori@gmail.com>
2020-05-21 14:52:52 -0400

    testing and changes

--- hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.test.js
+++ hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.test.js
@@ -3,14 +3,6 @@ import DashCard from "./DashCard";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-/** Testing the DashCard
- *  1. Does it render? Snapshot testing
- *  2. Does the hover effect work?
- *  3. does the clicking and the link work?
- *  4. Does the Icon component passed in render?
- *
- */
-
 describe("DashCard", () => {
     const testTitle = "any string";
     const testContent = [
