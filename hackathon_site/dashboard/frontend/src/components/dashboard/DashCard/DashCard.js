5e11b2eba3a3b6ec25bf30ab1b2d563ca2392cd0
Akinori Kimura <1999akinori@gmail.com>
2020-05-26 14:21:51 -0400

    2 changes requested by lisa

--- hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.js
+++ hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.js
@@ -18,7 +18,14 @@ const DashCard = ({ title, content }) => {
     return (
         <Container className={styles.DashCard} maxWidth={false} disableGutters={true}>
             <Typography variant="h2">{title}</Typography>
-            <Paper elevation={3} className={styles.paper} square={true}>
+            <Paper
+                elevation={3}
+                className={styles.paper}
+                square={true}
+                color="inherit"
+                underline="none"
+                target="_blank"
+            >
                 {content.map((listItem) => (
                     <Link href={listItem.url} rel="noopener">
                         <CardItem name={listItem.name} icon={listItem.icon} />
