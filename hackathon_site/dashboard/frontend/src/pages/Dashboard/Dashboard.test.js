eecbbb564d129d3ddbbc5b7ebaee2ea19e5b3fd6
Lisa Li <lisasiaili@gmail.com>
2020-05-26 21:52:03 -0400

    updated dashboard snapshot

--- hackathon_site/dashboard/frontend/src/pages/Dashboard/Dashboard.test.js
+++ hackathon_site/dashboard/frontend/src/pages/Dashboard/Dashboard.test.js
@@ -2,14 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Dashboard from "./Dashboard";
 
-it("renders correctly when the dashboard appears with no sponsor card", () => {
-    let sponsors = [{ imgSrc: "AMD.svg" }, { imgSrc: "CityofBrampton.svg" }];
-    const { asFragment } = render(<Dashboard sponsors={[]} />);
-    expect(asFragment()).toMatchSnapshot();
-});
-
 it("renders correctly when the dashboard appears with the sponsor card", () => {
-    let sponsors = [{ imgSrc: "AMD.svg" }, { imgSrc: "CityofBrampton.svg" }];
-    const { asFragment } = render(<Dashboard sponsors={sponsors} />);
+    const { asFragment } = render(<Dashboard />);
     expect(asFragment()).toMatchSnapshot();
 });
