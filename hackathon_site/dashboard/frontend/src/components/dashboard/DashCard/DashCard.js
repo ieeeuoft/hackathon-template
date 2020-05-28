5718e3189e2de69f6db125d27448c395f44d6f67
Akinori Kimura <1999akinori@gmail.com>
2020-05-28 15:28:55 -0400

    changed the attr from paper to link

--- hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.js
+++ hackathon_site/dashboard/frontend/src/components/dashboard/DashCard/DashCard.js
@@ -18,16 +18,15 @@ const DashCard = ({ title, content }) => {
     return (
         <Container className={styles.DashCard} maxWidth={false} disableGutters={true}>
             <Typography variant="h2">{title}</Typography>
-            <Paper
-                elevation={3}
-                className={styles.paper}
-                square={true}
-                color="inherit"
-                underline="none"
-                target="_blank"
-            >
+            <Paper elevation={3} className={styles.paper} square={true}>
                 {content.map((listItem) => (
-                    <Link href={listItem.url} rel="noopener">
+                    <Link
+                        href={listItem.url}
+                        rel="noopener"
+                        color="inherit"
+                        underline="none"
+                        target="_blank"
+                    >
                         <CardItem name={listItem.name} icon={listItem.icon} />
                     </Link>
                 ))}
