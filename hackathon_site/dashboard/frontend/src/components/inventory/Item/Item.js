b58cc46771f933267d94806228a42434a98e9dd0
Brenny <brennychao@gmail.com>
2020-06-05 21:57:06 -0400

    deleted folder component and created folder item

---
+++ hackathon_site/dashboard/frontend/src/components/inventory/Item/Item.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import styles from "./DashCard.module.scss";
+import Link from "@material-ui/core/Link";
+
+const ComponentCard = () => {};
