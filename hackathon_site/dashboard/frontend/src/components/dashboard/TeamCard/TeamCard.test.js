9e48b78249646e76a2240c6686a375926b8dbb06
Brenny <brennychao@gmail.com>
2020-06-05 21:10:17 -0400

    fixed test issues

--- hackathon_site/dashboard/frontend/src/components/dashboard/TeamCard/TeamCard.test.js
+++ hackathon_site/dashboard/frontend/src/components/dashboard/TeamCard/TeamCard.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import TeamCard from "./TeamCard";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { fireEvent } from "@testing-library/react";
 
 describe("TeamCard", () => {
     const members = ["Aki", "Graham", "Alex", "Raymond"];
@@ -23,8 +24,8 @@ describe("TeamCard", () => {
                 handleEditTeam={handleEditTeamSpy}
             />
         );
-        const button = getByText("Edit");
-        fireEvent.click(button);
+        const thebutton = getByText("Edit");
+        fireEvent.click(thebutton);
         expect(handleEditTeamSpy).toHaveBeenCalled();
     });
 });
