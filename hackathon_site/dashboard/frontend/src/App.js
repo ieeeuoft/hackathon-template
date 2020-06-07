8fedf5b2b65637c90c2892184a4cfb1c3f378c74
Graham Hoyes <accounts@grahamhoyes.com>
2020-06-07 14:21:46 -0400

    Plug our colors into MUI's theme

--- hackathon_site/dashboard/frontend/src/App.js
+++ hackathon_site/dashboard/frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+    createMuiTheme,
+    StylesProvider,
+    ThemeProvider,
+} from "@material-ui/core/styles";
 import { Provider as ReduxProvider } from "react-redux";
+import styles from "assets/abstracts/_exports.scss";
 
 import store from "slices/store";
 
@@ -9,6 +14,17 @@ import "App.scss";
 import Dashboard from "pages/Dashboard/Dashboard";
 import Footer from "components/general/Footer/Footer";
 import Login from "pages/Login/Login";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: styles.primary,
+        },
+        secondary: {
+            main: styles.secondary,
+        },
+    },
+});
 
 const UnconnectedApp = () => {
     return (
@@ -23,9 +39,11 @@ const UnconnectedApp = () => {
 const ConnectedApp = () => (
     <ReduxProvider store={store}>
         <StylesProvider injectFirst>
-            <Router>
-                <UnconnectedApp />
-            </Router>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <UnconnectedApp />
+                </Router>
+            </ThemeProvider>
         </StylesProvider>
     </ReduxProvider>
 );
