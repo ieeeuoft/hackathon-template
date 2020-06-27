3a33b187b149b528438a2b362296af97bfaec175
Alex Bogdan <stefanalexb@gmail.com>
2020-06-27 16:31:08 -0400

    Added quantity_remaining and unique hardware count to serializers for Hardware and Order

--- hackathon_site/hardware/serializers.py
+++ hackathon_site/hardware/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from hardware.models import Hardware, Category
+from hardware.models import Hardware, Category, Order
 
 
 class HardwareSerializer(serializers.ModelSerializer):
+    quantity_remaining = serializers.SerializerMethodField()
+
     class Meta:
         model = Hardware
         fields = (
@@ -16,12 +18,22 @@ class HardwareSerializer(serializers.ModelSerializer):
             "max_per_team",
             "picture",
             "categories",
+            "quantity_remaining"
         )
+
+
+    def get_quantity_remaining(self, obj):
+        return obj.quantity_available - Order.objects.filter(items__hardware__id=obj.id).count()
 
 
 class CategorySerializer(serializers.ModelSerializer):
     hardware = HardwareSerializer(many=True, read_only=True)
 
+    unique_hardware_count =  serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ("id", "name", "max_per_team", "hardware")
+        fields = ("id", "name", "max_per_team", "hardware", "unique_hardware_count")
+
+    def get_unique_hardware_count(self, obj):
+        return Hardware.objects.filter(categories__id=obj.id).count()
