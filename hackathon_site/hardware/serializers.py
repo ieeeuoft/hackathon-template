79cf8a99ab1e835ec0ea303b0deca93ae82d0a6a
Alex Bogdan <stefanalexb@gmail.com>
2020-06-27 16:35:25 -0400

    Black formatting

--- hackathon_site/hardware/serializers.py
+++ hackathon_site/hardware/serializers.py
@@ -18,18 +18,20 @@ class HardwareSerializer(serializers.ModelSerializer):
             "max_per_team",
             "picture",
             "categories",
-            "quantity_remaining"
+            "quantity_remaining",
         )
 
-
     def get_quantity_remaining(self, obj):
-        return obj.quantity_available - Order.objects.filter(items__hardware__id=obj.id).count()
+        return (
+            obj.quantity_available
+            - Order.objects.filter(items__hardware__id=obj.id).count()
+        )
 
 
 class CategorySerializer(serializers.ModelSerializer):
     hardware = HardwareSerializer(many=True, read_only=True)
 
-    unique_hardware_count =  serializers.SerializerMethodField()
+    unique_hardware_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
