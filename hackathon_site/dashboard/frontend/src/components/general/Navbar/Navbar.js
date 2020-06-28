82e233595e8dcb03deb577ea9c3fb31cf4541874
Lisa Li <lisasiaili@gmail.com>
2020-06-28 16:21:56 -0400

    added styling for active nav buttons

--- hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
+++ hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 // Images and logos
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import LocalMallIcon from "@material-ui/icons/LocalMall";
@@ -11,73 +12,81 @@ import { ReactComponent as Inventory } from "assets/images/icons/Hardware.svg";
 // Components
 import Button from "@material-ui/core/Button";
 
-const Navbar = ({ cartQuantity }) => {
-    return (
-        <nav className={styles.nav}>
-            <div className={styles.navFlexDiv}>
-                <Link to={"/"}>
-                    <Button
-                        className={styles.navBtn}
-                        aria-label="Dashboard"
-                        startIcon={<DashboardIcon />}
-                    >
-                        Dashboard
-                    </Button>
-                </Link>
-                <Link to={"/orders"}>
-                    <Button
-                        className={styles.navBtn}
-                        aria-label="Orders"
-                        startIcon={<ListAlt />}
-                    >
-                        Orders
-                    </Button>
-                </Link>
-                <Link to={"/teams"}>
-                    <Button
-                        className={styles.navBtn}
-                        aria-label="Teams"
-                        startIcon={<AccountBoxIcon />}
-                    >
-                        Teams
-                    </Button>
-                </Link>
-                <Link to={"/reports"}>
-                    <Button
-                        className={styles.navBtn}
-                        aria-label="Reports"
-                        startIcon={<InsertChartOutlined />}
-                    >
-                        Reports
-                    </Button>
-                </Link>
-                <Link to={"/inventory"}>
-                    <Button
-                        className={styles.navBtn}
-                        aria-label="Inventory"
-                        startIcon={<Inventory className={styles.customIcon} />}
-                    >
-                        Inventory
-                    </Button>
-                </Link>
-                <Link to={"/cart"}>
-                    <Button
-                        className={styles.navBtn}
-                        aria-label="Cart"
-                        startIcon={<LocalMallIcon />}
-                    >
-                        Cart ({cartQuantity})
-                    </Button>
-                </Link>
-            </div>
-            <Button
-                aria-label="Logout"
-                className={`${styles.navBtn} ${styles.navBtnLogout}`}
-            >
-                <b>Logout</b>
-            </Button>
-        </nav>
-    );
-};
+const Navbar = ({ cartQuantity, pathname }) => (
+    <nav className={styles.nav}>
+        <div className={styles.navFlexDiv}>
+            <Link to={"/"}>
+                <Button
+                    className={pathname === "/" ? styles.navActive : styles.navBtn}
+                    aria-label="Dashboard"
+                    startIcon={<DashboardIcon />}
+                >
+                    Dashboard
+                </Button>
+            </Link>
+            <Link to={"/orders"}>
+                <Button
+                    className={
+                        pathname === "/orders" ? styles.navActive : styles.navBtn
+                    }
+                    aria-label="Orders"
+                    startIcon={<ListAlt />}
+                >
+                    Orders
+                </Button>
+            </Link>
+            <Link to={"/teams"}>
+                <Button
+                    className={pathname === "/teams" ? styles.navActive : styles.navBtn}
+                    aria-label="Teams"
+                    startIcon={<AccountBoxIcon />}
+                >
+                    Teams
+                </Button>
+            </Link>
+            <Link to={"/reports"}>
+                <Button
+                    className={
+                        pathname === "/reports" ? styles.navActive : styles.navBtn
+                    }
+                    aria-label="Reports"
+                    startIcon={<InsertChartOutlined />}
+                >
+                    Reports
+                </Button>
+            </Link>
+            <Link to={"/inventory"}>
+                <Button
+                    className={
+                        pathname === "/inventory" ? styles.navActive : styles.navBtn
+                    }
+                    aria-label="Inventory"
+                    startIcon={<Inventory fill="currentColor" width="20px" />}
+                >
+                    Inventory
+                </Button>
+            </Link>
+            <Link to={"/cart"}>
+                <Button
+                    className={pathname === "/cart" ? styles.navActive : styles.navBtn}
+                    aria-label="Cart"
+                    startIcon={<LocalMallIcon />}
+                >
+                    Cart ({cartQuantity})
+                </Button>
+            </Link>
+        </div>
+        <Button
+            aria-label="Logout"
+            className={`${styles.navBtn} ${styles.navBtnLogout}`}
+        >
+            <b>Logout</b>
+        </Button>
+    </nav>
+);
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+    pathname: state.router.location.pathname,
+});
+
+export default connect(mapStateToProps)(Navbar);
--- hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.test.js
+++ hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.test.js
@@ -5,16 +5,7 @@ import { withRouter } from "testing/helpers";
 
 describe("<Navbar />", () => {
     it("renders correctly when all icons appear", () => {
-        const { asFragment } = render(
-            withRouter(<Navbar cartQuantity={1} unreadNotificationQuantity={1} />)
-        );
-        expect(asFragment()).toMatchSnapshot();
-    });
-
-    it("renders correctly when all icons appear but no badge on notification iconon mobile", () => {
-        const { asFragment } = render(
-            withRouter(<Navbar cartQuantity={0} unreadNotificationQuantity={0} />)
-        );
+        const { asFragment } = render(withRouter(<Navbar cartQuantity={1} />));
         expect(asFragment()).toMatchSnapshot();
     });
 });
