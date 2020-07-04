6f3dc50f91c8c064a9c7001d95aa3e7bf321ad1b
Graham Hoyes <accounts@grahamhoyes.com>
2020-07-03 22:24:53 -0400

    Add serializers, views, routes

---
+++ hackathon_site/event/serializers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from rest_framework import serializers
+
+from event.models import Profile
+
+User = get_user_model()
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ("id", "name")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email", "groups")
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = (
+            "id",
+            "user",
+            "status",
+            "id_provided",
+            "attended",
+            "acknowledge_rules",
+            "e_signature",
+        )
--- hackathon_site/event/views.py
+++ hackathon_site/event/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
 
-# Create your views here.
+from event.models import Profile
+from event.serializers import ProfileSerializer
+
+
+class CurrentUserAPIView(generics.GenericAPIView, mixins.RetrieveModelMixin):
+    """
+    View to handle API interaction with the current user's Profile
+    """
+
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        """
+        Get the current user's profile and user details
+
+        Reads the profile of the current logged in user. User details and
+        group list are nested within the profile and user object, respectively.
+        """
+        return self.retrieve(request, *args, **kwargs)
