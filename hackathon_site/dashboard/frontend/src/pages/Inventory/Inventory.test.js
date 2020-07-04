450522a3a4e77603928e8ee4906c9aed8b8ff7ee
Lisa Li <42653157+lisa-sa-li@users.noreply.github.com>
2020-07-04 17:49:35 -0400

    Connected Navbar to store to get pathname. Navbar button is active if you're on that page. (#60)
    * added styling for active nav buttons
    
    * updated tests to include store
    
    * updated tests for nav components
    
    * applied prettier
    
    * changed color of light blue for active nav
    
    * fixed navbar test
    
    * added variables to test name

--- hackathon_site/dashboard/frontend/src/pages/Inventory/Inventory.test.js
+++ hackathon_site/dashboard/frontend/src/pages/Inventory/Inventory.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Inventory from "./Inventory";
-import { withRouter } from "testing/helpers";
+import { withStoreAndRouter } from "testing/helpers";
 
 test("renders without crashing", () => {
-    const { getByText } = render(withRouter(<Inventory />));
+    const { getByText } = render(withStoreAndRouter(<Inventory />));
     expect(getByText("IEEEEEE")).toBeInTheDocument();
 });
