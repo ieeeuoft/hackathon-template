9645b3fedfac42a25f7f157c4f7d7a39e94718b5
Graham Hoyes <accounts@grahamhoyes.com>
2020-07-04 16:38:40 -0400

    Enable API pagination

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -124,7 +124,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
 }
 
 # Internationalization
