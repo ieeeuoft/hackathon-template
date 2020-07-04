abe567636bdb50ee7f7ac578d6697b4ac78fd600
Graham Hoyes <accounts@grahamhoyes.com>
2020-07-04 16:37:38 -0400

    Make token default auth class

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -122,7 +122,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication"
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ]
 }
 
