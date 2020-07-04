3caca6138d7f635f58b9c6f2a6d3629c79f89309
Graham Hoyes <accounts@grahamhoyes.com>
2020-07-03 21:33:12 -0400

    UI Redux Slice (#32)
    * Dashboard reducer and actions
    
    * dashboardSlice -> uiSlice
    
    * Fix existing tests
    
    * Redux tests
    
    * Component tests
    
    * Fix a warning
    
    * Remove commented out tests

--- hackathon_site/dashboard/frontend/src/pages/Dashboard/Dashboard.test.js
+++ hackathon_site/dashboard/frontend/src/pages/Dashboard/Dashboard.test.js
@@ -2,10 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Dashboard from "./Dashboard";
 import { cardItems } from "testing/mockData";
-import { withRouter } from "testing/helpers";
+import { withRouter, withStore } from "testing/helpers";
 
-it("renders correctly when the dashboard appears 4 cards and 3 tables", () => {
-    const { queryByText, getByText } = render(withRouter(<Dashboard />));
+it("Renders correctly when the dashboard appears 4 cards and 3 tables", () => {
+    const { queryByText, getByText } = render(withStore(withRouter(<Dashboard />)));
     for (let e of cardItems) {
         expect(queryByText(e.title)).toBeTruthy();
     }
