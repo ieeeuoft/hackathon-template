fd7765d5be0333093baaa0c5395f29cef6634ef3
Lisa Li <lisasiaili@gmail.com>
2020-07-06 15:51:16 -0400

    added filter component and test

---
+++ hackathon_site/dashboard/frontend/src/components/inventory/InventoryFilter/InventoryFilter.js
@@ -0,0 +1,85 @@
+import React from "react";
+import styles from "./InventoryFilter.module.scss";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+import Divider from "@material-ui/core/Divider";
+import Checkbox from "@material-ui/core/Checkbox";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+const InventoryFilter = ({ categories, applyFilter, removeFilter }) => (
+    <div className={styles.filter}>
+        <Paper className={styles.filterPaper}>
+            <form>
+                <Typography variant="h2" noWrap>
+                    Order by
+                </Typography>
+                <RadioGroup aria-label="order by" name="order by">
+                    <FormControlLabel
+                        value="all"
+                        control={<Radio color="primary" />}
+                        label="All"
+                    />
+                    <FormControlLabel
+                        value="a-z"
+                        control={<Radio color="primary" />}
+                        label="A-Z"
+                    />
+                    <FormControlLabel
+                        value="z-a"
+                        control={<Radio color="primary" />}
+                        label="Z-A"
+                    />
+                    <FormControlLabel
+                        value="stock-high-low"
+                        control={<Radio color="primary" />}
+                        label="Stock remaining: high to low"
+                    />
+                    <FormControlLabel
+                        value="stock-low-high"
+                        control={<Radio color="primary" />}
+                        label="Stock remaining: low to high"
+                    />
+                </RadioGroup>
+                <Divider className={styles.filterDivider} />
+                <Typography variant="h2" noWrap>
+                    Availability
+                </Typography>
+                <FormControlLabel
+                    control={<Checkbox color="primary" />}
+                    label="In stock"
+                />
+                <Divider className={styles.filterDivider} />
+                <Typography variant="h2" noWrap>
+                    Categories
+                </Typography>
+                {categories.map((item, i) => (
+                    <div className={styles.filterCategory} key={i}>
+                        <FormControlLabel
+                            control={<Checkbox color="primary" />}
+                            label={item.name}
+                        />
+                        <Chip label={item.qty} className={styles.filterCategoryChip} />
+                    </div>
+                ))}
+            </form>
+        </Paper>
+        <Button
+            color="primary"
+            variant="contained"
+            fullWidth={true}
+            onClick={applyFilter}
+            className={styles.filterBtn}
+        >
+            Apply
+        </Button>
+        <Button color="secondary" onClick={removeFilter}>
+            Clear all
+        </Button>
+    </div>
+);
+
+export default InventoryFilter;
