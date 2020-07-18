bea4807110884c6241be4fff836ea92b1b1e8ed4
Lisa Li <42653157+lisa-sa-li@users.noreply.github.com>
2020-07-18 16:00:14 -0400

    Refactored DashCard, TeamCard, CartSummary to create TitledPaper (#74)
    * added component
    
    * added test for component
    
    * minor formatting change
    
    * refactored DashCard and TeamCard to create TitledPaper
    
    * updated snapshot
    
    * updated snapshot again
    
    * updated keys and removed snapshots
    
    * edited keys

---
+++ hackathon_site/dashboard/frontend/src/components/general/TitledPaper/TitledPaper.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./TitledPaper.module.scss";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+
+const TitledPaper = ({ title, children }) => (
+    <>
+        {title && (
+            <Typography
+                variant="h2"
+                className={styles.title}
+                data-testid="titledPaperTitle"
+                noWrap
+            >
+                {title}
+            </Typography>
+        )}
+        <Paper elevation={3} square={true} className={styles.paper}>
+            {children}
+        </Paper>
+    </>
+);
+
+export default TitledPaper;
