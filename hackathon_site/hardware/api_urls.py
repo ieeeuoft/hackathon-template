bd6b3fe65633dc8b868c9b70759e7fa3deeb7634
Alex Bogdan <stefanalexb@gmail.com>
2020-08-01 18:14:14 -0400

    Fixed url patterns

--- hackathon_site/hardware/api_urls.py
+++ hackathon_site/hardware/api_urls.py
@@ -5,6 +5,6 @@ from hardware import views
 app_name = "hardware"
 
 urlpatterns = [
-    path("hardware/", views.HardwareListView.as_view(), name="hardware-list", ),
+    path("hardware/", views.HardwareListView.as_view(), name="hardware-list",),
     path("categories/", views.CategoryListView.as_view(), name="category-list"),
 ]
