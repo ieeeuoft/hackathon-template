d016c3f0e5ca0e7508ba1416cd9e133c8724ceb8
Graham Hoyes <accounts@grahamhoyes.com>
2020-08-07 18:27:31 -0400

    Delete the applications app

--- hackathon_site/applications/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Application, Team as TeamApplied
-
-# Register your models here.
-admin.site.register(Application)
-admin.site.register(TeamApplied)
