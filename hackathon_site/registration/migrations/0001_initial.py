b622797daea21fa6b5c1c6895f5dd92e62338542
Graham Hoyes <accounts@grahamhoyes.com>
2020-08-07 18:30:48 -0400

    Black formatting

--- hackathon_site/registration/migrations/0001_initial.py
+++ hackathon_site/registration/migrations/0001_initial.py
@@ -16,32 +16,65 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Team',
+            name="Team",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('team_code', models.CharField(default=registration.models._generate_team_code, max_length=5)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "team_code",
+                    models.CharField(
+                        default=registration.models._generate_team_code, max_length=5
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
             ],
         ),
         migrations.CreateModel(
-            name='Application',
+            name="Application",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('preferred_name', models.CharField(max_length=255)),
-                ('birthday', models.DateField()),
-                ('gender', models.CharField(max_length=50)),
-                ('phone_number', models.CharField(max_length=20)),
-                ('resume', models.FileField(upload_to='applications/resumes/')),
-                ('q1', models.TextField()),
-                ('q2', models.TextField()),
-                ('q3', models.TextField()),
-                ('mlh_conduct_agree', models.BooleanField(default=False)),
-                ('mlh_data_agree', models.BooleanField(default=False)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('team', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='registration.Team')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("preferred_name", models.CharField(max_length=255)),
+                ("birthday", models.DateField()),
+                ("gender", models.CharField(max_length=50)),
+                ("phone_number", models.CharField(max_length=20)),
+                ("resume", models.FileField(upload_to="applications/resumes/")),
+                ("q1", models.TextField()),
+                ("q2", models.TextField()),
+                ("q3", models.TextField()),
+                ("mlh_conduct_agree", models.BooleanField(default=False)),
+                ("mlh_data_agree", models.BooleanField(default=False)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "team",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="registration.Team",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
