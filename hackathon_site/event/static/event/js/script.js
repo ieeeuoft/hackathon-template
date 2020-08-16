c62fc9dde6c652eea4b737f25eb1167409bd0038
Lisa Li <lisasiaili@gmail.com>
2020-08-16 14:14:53 -0400

    ran prettier

--- hackathon_site/event/static/event/js/script.js
+++ hackathon_site/event/static/event/js/script.js
@@ -1,27 +1,26 @@
 // Change navbar color on scroll
 $(document).scroll(function () {
     let $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
 });
 
-
-$(document).ready(function(){
+$(document).ready(function () {
     // Materialize stuff
-    $('.sidenav').sidenav();
-    $('.carousel').carousel({dist: 0, padding: 600 });
+    $(".sidenav").sidenav();
+    $(".carousel").carousel({ dist: 0, padding: 600 });
 
-    setInterval(function() {
-        $('.carousel').carousel('next');
-    } , 3000); 
+    setInterval(function () {
+        $(".carousel").carousel("next");
+    }, 3000);
 
-    $('.scrollspy').scrollSpy();
-    $('.collapsible').collapsible();
+    $(".scrollspy").scrollSpy();
+    $(".collapsible").collapsible();
 
     // Countdown stuff
     let countDownDate = new Date("Dec 31, 2020 00:00:00").getTime();
 
     // Update the count down every hour
-    let x = setInterval(function() {
+    let x = setInterval(function () {
         let now = new Date().getTime();
         let distance = countDownDate - now;
         let days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -34,7 +33,7 @@ $(document).ready(function(){
 
         $("#day2").html(Math.floor(days / 10) % 10);
         $("#day3").html(days % 10);
-        
+
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
@@ -43,5 +42,3 @@ $(document).ready(function(){
         }
     }, 1000);
 });
-
-    
