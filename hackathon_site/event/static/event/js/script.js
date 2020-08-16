f022a0132e25332bb308f9b32f8e954d3472d78a
Lisa Li <lisasiaili@gmail.com>
2020-08-16 18:06:00 -0400

    edited countdown logic

--- hackathon_site/event/static/event/js/script.js
+++ hackathon_site/event/static/event/js/script.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
     // Materialize stuff
     $(".sidenav").sidenav();
     $(".carousel").carousel({ dist: 0, padding: 600 });
-
     setInterval(function () {
         $(".carousel").carousel("next");
     }, 3000);
@@ -17,28 +16,48 @@ $(document).ready(function () {
     $(".collapsible").collapsible();
 
     // Countdown stuff
-    let countDownDate = new Date("Dec 31, 2020 00:00:00").getTime();
 
-    // Update the count down every hour
-    let x = setInterval(function () {
-        let now = new Date().getTime();
-        let distance = countDownDate - now;
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    // Registration open and close date, and event date
+    let regOpenDate = new Date("Sep 1, 2020 00:00:00").getTime();
+    let regCloseDate = new Date("Sep 30, 2020 00:00:00").getTime();
+    let eventDate = new Date("Oct 31, 2020 00:00:00").getTime();
 
-        if (days > 100) {
-            $("#day1").html(Math.floor(days / 100));
-        } else {
-            $("#day1").parent().remove();
-        }
+    let now = new Date().getTime();
 
-        $("#day2").html(Math.floor(days / 10) % 10);
-        $("#day3").html(days % 10);
+    // Set the title based off what it's counting down to
+    if (regOpenDate > now) {
+        countDownDate = regOpenDate;
+        $("#countdownTitle").html("Registration Opens In");
+    } else if (regCloseDate > now) {
+        countDownDate = regCloseDate;
+        $("#countdownTitle").html("Registration Closes In");
+    } else if (eventDate > now) {
+        countDownDate = eventDate;
+        $("#countdownTitle").html("Event Starts In");
+    }
 
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            $("#day2").html(0);
-            $("#day3").html(0);
-        }
-    }, 1000);
+    // Delete the entire countdown if event start date has passed
+    if (eventDate < now) {
+        $("#countdown").remove();
+        $("#aboutText").removeClass("l7");
+    } else {
+        // Update the countdown every ten minute
+        setInterval(setCounter(countDownDate), 600000);
+    }
 });
+
+function setCounter(countDownDate) {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.ceil(distance / (1000 * 60 * 60 * 24));
+
+    // Check if we need a third digit or not
+    if (days > 99) {
+        $("#day1").html(Math.floor(days / 100));
+    } else {
+        $("#day1").parent().remove();
+    }
+
+    $("#day2").html(Math.floor(days / 10) % 10);
+    $("#day3").html(days % 10);
+}
