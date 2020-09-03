5c43ebc7a61a5e9f237aff63f709f9cc5ab0670f
Brenny <brennychao@gmail.com>
2020-09-03 15:06:59 -0400

    Finished the form without using form control, added some tests

--- hackathon_site/dashboard/frontend/src/components/user/AcknowledgementForm/AcknowledgementForm.js
+++ hackathon_site/dashboard/frontend/src/components/user/AcknowledgementForm/AcknowledgementForm.js
@@ -6,19 +6,47 @@ import TextField from "@material-ui/core/TextField/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
 import Button from "@material-ui/core/Button/Button";
 import Link from "@material-ui/core/Link/Link";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field } from "formik";
 
 export const ERROR_MESSAGES = {
     signatureMissing: "e-signature is required",
     checkboxMissing: "All checkboxes are required",
 };
 
-const AcknowledgementForm = ({ handleContinue, openDoc }) => {
+export const FormikCheckBox = (props) => {
+    return (
+        <Field name={props.name}>
+            {({ field, form }) => (
+                <label>
+                    <input
+                        type="checkbox"
+                        {...props}
+                        checked={field.value.includes(props.value)}
+                        onChange={() => {
+                            if (field.value.includes(props.value)) {
+                                const nextValue = field.value.filter(
+                                    (value) => value !== props.value
+                                );
+                                form.setFieldValue(props.name, nextValue);
+                            } else {
+                                const nextValue = field.value.concat(props.value);
+                                form.setFieldValue(props.name, nextValue);
+                            }
+                        }}
+                    />
+                    {props.value}
+                </label>
+            )}
+        </Field>
+    );
+};
+
+export const AcknowledgementForm = ({ handleContinue, openDoc }) => {
     const checkBox1 =
         "I understand that making a request does not guarantee hardware. Hardware is given on a first-come-first-serve basis.";
     const checkBox2 =
         "Each member of the team must provide government-issued photo ID to check out components. ID will be returned when all components are returned.";
-    const checkBox3 = "I cannot keep hardware/components lent out to me.";
+    const checkBox3 = " I cannot keep hardware/components lent out to me.";
     const checkBox4 =
         "I will be held accountable for damaged or lost hardware. The handling of each instance is case by case.";
     const firstHalf =
@@ -27,22 +55,65 @@ const AcknowledgementForm = ({ handleContinue, openDoc }) => {
         ", understand it and sign it voluntarily as my own free act and deed; no oral representations, statements, or inducements, apart from the foregoing written agreement, have been made; I am at least eighteen (18) years of age and fully competent; and I execute this Release for full, adequate and complete consideration fully intending to be bound by same.";
 
     return (
-        <form className={styles.acknowledge} onSubmit={handleContinue}>
-            <FormControlLabel
-                control={<Checkbox name="firstCheck" />}
-                label={checkBox1}
+        // <form className={styles.acknowledge} onSubmit={handleContinue}>
+        //     <FormControlLabel
+        //         control={<Checkbox name="firstCheck" />}
+        //         label={checkBox1}
+        //     />
+        //     <FormControlLabel
+        //         control={<Checkbox name="secondCheck" />}
+        //         label={checkBox2}
+        //     />
+        //     <FormControlLabel
+        //         control={<Checkbox name="thirdCheck" />}
+        //         label={checkBox3}
+        //     />
+        //     <FormControlLabel
+        //         control={<Checkbox name="forthCheck" />}
+        //         label={checkBox4}
+        //     />
+        //     <Typography variant="body1" className={styles.acknowledgeText}>
+        //         {firstHalf}
+        //         <Link href="#" onClick={openDoc}>
+        //             Waiver of Liability and Hold Harmless Agreement
+        //         </Link>
+        //         {secondHalf}
+        //     </Typography>
+        //     <TextField
+        //         label="e-signature"
+        //         className={styles.acknowledgeSignature}
+        //         name="signature"
+        //     />
+        //     <Button
+        //         type="submit"
+        //         className={styles.acknowledgeButton}
+        //         variant="contained"
+        //         color="primary"
+        //         onClick={handleContinue}
+        //     >
+        //         Continue
+        //     </Button>
+        // </form>
+        <div className={styles.acknowledge} onSubmit={handleContinue}>
+            <FormikCheckBox
+                name="agreement"
+                value={checkBox1}
+                className={styles.acknowledgeCheckbox}
             />
-            <FormControlLabel
-                control={<Checkbox name="secondCheck" />}
-                label={checkBox2}
+            <FormikCheckBox
+                name="agreement"
+                value={checkBox2}
+                className={styles.acknowledgeCheckbox}
             />
-            <FormControlLabel
-                control={<Checkbox name="thirdCheck" />}
-                label={checkBox3}
+            <FormikCheckBox
+                name="agreement"
+                value={checkBox3}
+                className={styles.acknowledgeCheckbox}
             />
-            <FormControlLabel
-                control={<Checkbox name="forthCheck" />}
-                label={checkBox4}
+            <FormikCheckBox
+                name="agreement"
+                value={checkBox4}
+                className={styles.acknowledgeCheckbox}
             />
             <Typography variant="body1" className={styles.acknowledgeText}>
                 {firstHalf}
@@ -65,22 +136,22 @@ const AcknowledgementForm = ({ handleContinue, openDoc }) => {
             >
                 Continue
             </Button>
-        </form>
+        </div>
     );
 };
 
 export const EnhancedAcknowledgmentForm = ({
-    handleSignIn,
-    isLoading,
+    handleSubmit,
     requestFailure,
+    openDoc,
 }) => {
     const onSubmit = (formikValues) => {
-        handleSignIn({ email: formikValues.email, password: formikValues.password });
+        handleSubmit({ signature: formikValues.signature });
     };
 
     return (
         <Formik
-            initialValues={{ signature: "" }}
+            initialValues={{ signature: "", agreement: [""] }}
             onSubmit={onSubmit}
             validateOnBlur={false}
             validateOnChange={false}
@@ -88,7 +159,8 @@ export const EnhancedAcknowledgmentForm = ({
             {(formikProps) => (
                 <AcknowledgementForm
                     {...formikProps}
-                    isLoading={isLoading}
+                    openDoc={openDoc}
+                    handleContinue={onSubmit}
                     requestFailure={requestFailure}
                 />
             )}
--- hackathon_site/dashboard/frontend/src/components/user/AcknowledgementForm/AcknowledgementForm.test.js
+++ hackathon_site/dashboard/frontend/src/components/user/AcknowledgementForm/AcknowledgementForm.test.js
@@ -1,1 +1,74 @@
 import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { EnhancedAcknowledgmentForm } from "./AcknowledgementForm";
+
+const waiver = "Waiver of Liability and Hold Harmless Agreement";
+
+describe("<EnhancedAcknowledgmentForm />", () => {
+    test("Check if all the text are present", () => {
+        const handleSubmit = () => {};
+        const requestFailure = () => {};
+        const openDoc = () => {};
+
+        const { getByText } = render(
+            <EnhancedAcknowledgmentForm
+                handleSubmit={handleSubmit}
+                requestFailure={requestFailure}
+                openDoc={openDoc}
+            />
+        );
+
+        expect(getByText(/Continue/i)).toBeInTheDocument();
+        expect(getByText(/e-signature/i)).toBeInTheDocument();
+        expect(
+            getByText(
+                /I understand that making a request does not guarantee hardware. Hardware is given on a first-come-first-serve basis./i
+            )
+        ).toBeInTheDocument();
+        expect(
+            getByText(
+                /Each member of the team must provide government-issued photo ID to check out components. ID will be returned when all components are returned./i
+            )
+        ).toBeInTheDocument();
+        expect(
+            getByText(/I cannot keep hardware\/components lent out to me/i)
+        ).toBeInTheDocument();
+        expect(
+            getByText(
+                /I will be held accountable for damaged or lost hardware. The handling of each instance is case by case./i
+            )
+        ).toBeInTheDocument();
+        expect(
+            getByText(
+                /IN SIGNING THIS RELEASE, I ACKNOWLEDGE AND REPRESENT THAT I have read the foregoing/i
+            )
+        ).toBeInTheDocument();
+        expect(getByText(waiver)).toBeInTheDocument();
+        expect(
+            getByText(
+                /, understand it and sign it voluntarily as my own free act and deed; no oral representations, statements, or inducements, apart from the foregoing written agreement, have been made; I am at least eighteen \(18\) years of age and fully competent; and I execute this Release for full, adequate and complete consideration fully intending to be bound by same./i
+            )
+        ).toBeInTheDocument();
+    });
+
+    test("Check if the submit button is called when pressed, and if the openDoc function gets called for the link", () => {
+        const handleSubmit = jest.fn();
+        const requestFailure = () => {};
+        const openDoc = jest.fn();
+
+        const { getByText } = render(
+            <EnhancedAcknowledgmentForm
+                handleSubmit={handleSubmit}
+                requestFailure={requestFailure}
+                openDoc={openDoc}
+            />
+        );
+
+        const submitButton = getByText(/Continue/i);
+        fireEvent.click(submitButton);
+        expect(handleSubmit).toHaveBeenCalled();
+        const waiverLink = getByText(waiver);
+        fireEvent.click(waiverLink);
+        expect(openDoc).toHaveBeenCalled();
+    });
+});
