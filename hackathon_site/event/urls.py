3cb6906a761e3896200ea8ae840310b4c909d1df
Graham Hoyes <accounts@grahamhoyes.com>
2020-09-04 23:22:47 -0400

    Login page (#138)
    * Login template
    
    * Basic tests
    
    * Add a note about tests
    
    * Black formatting

--- hackathon_site/event/urls.py
+++ hackathon_site/event/urls.py
@@ -1,6 +1,14 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from event.views import IndexView
 
 app_name = "event"
 
-urlpatterns = [path("", IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="event/login.html"),
+        name="login",
+    ),
+]
