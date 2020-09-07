1291a3f95f75774ed38d4ce58c90a26c45c46d92
Alex Bogdan <stefanalexb@gmail.com>
2020-09-07 18:25:09 -0400

    Minor fixes addressing comments, added Dashboard link to navbar, made logo a link

--- hackathon_site/event/tests.py
+++ hackathon_site/event/tests.py
@@ -55,8 +55,8 @@ class LogInViewTestCase(SetupUserMixin, TestCase):
     """
     Tests for the login template.
 
-    This view uses django.contrib.auth.forms.AuthenticationForm, so no direct
-    form testing is required.
+    This view uses django.contrib.auth.views.LogoutView, so no logic testing
+    is required
 
     Ideally, selenium should be used for a full test of the view UI.
     For practicality reasons, tests for templates with forms are limited to
