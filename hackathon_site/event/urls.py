940151d54b287a80014540ed5cff1aff82902cbe
Alex Bogdan <31147673+albogdan@users.noreply.github.com>
2020-09-08 14:10:10 -0400

    142 - Password Change when Logged In (#147)
    * Added PasswordChange and PasswordChangeDone views to the event urls
    
    * Subclasses PasswordChangeForm in order to apply custom formatting
    
    * Added change password and change password done jinja templates
    
    * Added tests for changing password
    
    * Added change password link to dashboard navbar
    
    * Fixed a link to create an account from the login page
    
    * Fixed change password button text
    
    * Fixed suggestions
    
    * Fixed spacing

--- hackathon_site/event/urls.py
+++ hackathon_site/event/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 from event.views import IndexView, DashboardView
+from event.forms import PasswordChangeForm
 
 app_name = "event"
 
@@ -15,4 +16,20 @@ urlpatterns = [
         "accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout",
     ),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path(
+        "accounts/change_password/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="event/change_password.html",
+            success_url=reverse_lazy("event:change_password_done"),
+            form_class=PasswordChangeForm,
+        ),
+        name="change_password",
+    ),
+    path(
+        "accounts/change_password_done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="event/change_password_done.html"
+        ),
+        name="change_password_done",
+    ),
 ]
