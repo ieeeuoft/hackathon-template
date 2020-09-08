5f1b2ad5f824ddc1ec0a1e3b6232934e4eb1bd0d
Alex Bogdan <stefanalexb@gmail.com>
2020-09-07 21:23:18 -0400

    Added PasswordChange and PasswordChangeDone views to the event urls

--- hackathon_site/event/urls.py
+++ hackathon_site/event/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from event.views import IndexView, DashboardView
+from django.urls import reverse_lazy
+from event.forms import PasswordChangeForm
 
 app_name = "event"
 
@@ -15,4 +17,20 @@ urlpatterns = [
         "accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout",
     ),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path(
+        "accounts/change_password/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="event/change_password.html",
+            success_url=reverse_lazy("event:change_password_done"),
+            form_class=PasswordChangeForm,
+        ),
+        name="change_password",
+    ),
+    path(
+        "accounts/change_password_done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="event/change_password_done.html"
+        ),
+        name="change_password_done",
+    ),
 ]
