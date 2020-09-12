9c4f0220ca048b10a8b86fc556217e9aedf99f58
Graham Hoyes <accounts@grahamhoyes.com>
2020-09-12 16:32:57 -0400

    Join team form

--- hackathon_site/registration/test_forms.py
+++ hackathon_site/registration/test_forms.py
@@ -1,13 +1,13 @@
 from datetime import date
 from io import BytesIO
 
-from django.core.files.uploadedfile import SimpleUploadedFile, InMemoryUploadedFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.test import TestCase
 from django_registration import validators
 
 from hackathon_site.tests import SetupUserMixin
 from registration.models import User, Team, Application
-from registration.forms import SignUpForm, ApplicationForm
+from registration.forms import SignUpForm, ApplicationForm, JoinTeamForm
 
 
 class SignUpFormTestCase(TestCase):
@@ -233,3 +233,35 @@ class ApplicationFormTestCase(SetupUserMixin, TestCase):
             "File must be no bigger than 20.0\xa0MB. Currently 20.0\xa0MB.",
             form.errors["resume"],
         )
+
+
+class JoinTeamFormTestCase(SetupUserMixin, TestCase):
+    def setUp(self):
+        super().setUp()
+        self.team = Team.objects.create(team_code="AAAAA")
+
+    def test_team_not_found(self):
+        form = JoinTeamForm(data={"team_code": "BBBBB"})
+        self.assertFalse(form.is_valid())
+        self.assertIn("Team BBBBB does not exist.", form.errors["team_code"])
+
+    def test_team_full(self):
+        user2 = User.objects.create_user(username="bob@ross.com", password="abcdef123")
+        user3 = User.objects.create_user(
+            username="franklin@carmichael", password="supersecret456"
+        )
+        user4 = User.objects.create_user(username="lawren@harris", password="wxyz7890")
+
+        self._apply_as_user(self.user, self.team)
+        self._apply_as_user(user2, self.team)
+        self._apply_as_user(user3, self.team)
+        self._apply_as_user(user4, self.team)
+
+        form = JoinTeamForm(data={"team_code": self.team.team_code})
+        self.assertFalse(form.is_valid())
+        self.assertIn(f"Team {self.team.team_code} is full.", form.errors["team_code"])
+
+    def test_team_has_space(self):
+        self._apply_as_user(self.user, self.team)
+        form = JoinTeamForm(data={"team_code": self.team.team_code})
+        self.assertTrue(form.is_valid())
