ca917a5ef225bbefcee04f6ea1e267ce733c0073
Lisa Li <lisasiaili@gmail.com>
2020-09-12 23:07:19 -0400

    fixed grahams comments

--- hackathon_site/registration/migrations/0003_auto_20200912_2204.py
+++ hackathon_site/registration/migrations/0003_auto_20200912_2204.py
@@ -6,23 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0002_auto_20200910_1606'),
+        ("registration", "0002_auto_20200910_1606"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='application',
-            name='q1',
-            field=models.TextField(help_text='First question?', max_length=1000),
+            model_name="application",
+            name="q1",
+            field=models.TextField(help_text="First question?", max_length=1000),
         ),
         migrations.AlterField(
-            model_name='application',
-            name='q2',
-            field=models.TextField(help_text='Second question?', max_length=1000),
+            model_name="application",
+            name="q2",
+            field=models.TextField(help_text="Second question?", max_length=1000),
         ),
         migrations.AlterField(
-            model_name='application',
-            name='q3',
-            field=models.TextField(help_text='Third question?', max_length=1000),
+            model_name="application",
+            name="q3",
+            field=models.TextField(help_text="Third question?", max_length=1000),
         ),
     ]
