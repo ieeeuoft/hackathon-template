6b4c017e86dad679012adaa9ef4a6f01aac50207
Graham Hoyes <accounts@grahamhoyes.com>
2020-09-13 22:35:13 -0400

    Add logout redirect url to settings

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -113,6 +113,7 @@ FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 WSGI_APPLICATION = "hackathon_site.wsgi.application"
 
 LOGIN_REDIRECT_URL = reverse_lazy("event:dashboard")
+LOGOUT_REDIRECT_URL = reverse_lazy("event:index")
 
 
 # Database
