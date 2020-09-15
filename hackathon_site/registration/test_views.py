88a676779c620f9f281ae339a57c2f0ea6e5ebc2
Graham Hoyes <accounts@grahamhoyes.com>
2020-09-15 14:28:44 -0400

    Fix tests

--- hackathon_site/registration/test_views.py
+++ hackathon_site/registration/test_views.py
@@ -74,6 +74,9 @@ class SignUpClosedViewTestCase(TestCase):
         mock_now.return_value = datetime(2020, 1, 1, tzinfo=settings.TZ_INFO)
         response = self.client.get(self.view)
         self.assertContains(response, "Applications have not opened yet")
+        self.assertContains(
+            response, settings.REGISTRATION_OPEN_DATE.strftime("%B %-d, %Y")
+        )
 
     @override_settings(
         REGISTRATION_OPEN_DATE=datetime(2020, 1, 1, tzinfo=settings.TZ_INFO)
@@ -86,6 +89,7 @@ class SignUpClosedViewTestCase(TestCase):
         mock_now.return_value = datetime(2020, 1, 2, tzinfo=settings.TZ_INFO)
         response = self.client.get(self.view)
         self.assertContains(response, "Applications are open!")
+        self.assertContains(response, reverse("registration:signup"))
 
     @override_settings(
         REGISTRATION_OPEN_DATE=datetime(2020, 1, 1, tzinfo=settings.TZ_INFO)
@@ -98,6 +102,9 @@ class SignUpClosedViewTestCase(TestCase):
         mock_now.return_value = datetime(2020, 1, 3, tzinfo=settings.TZ_INFO)
         response = self.client.get(self.view)
         self.assertContains(response, "Applications have closed")
+        self.assertContains(
+            response, settings.REGISTRATION_CLOSE_DATE.strftime("%B %-d, %Y")
+        )
 
 
 class ActivationViewTestCase(SetupUserMixin, TestCase):
@@ -218,16 +225,6 @@ class MiscRegistrationViewsTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Test that the default from email was passed in as a context variable
         self.assertContains(response, settings.DEFAULT_FROM_EMAIL)
-
-    def test_signup_closed(self):
-        response = self.client.get(reverse("registration:signup_closed"))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Test that context variables were passed in
-        self.assertContains(response, settings.HACKATHON_NAME)
-        self.assertContains(response, settings.CONTACT_EMAIL)
-        self.assertContains(
-            response, settings.REGISTRATION_CLOSE_DATE.strftime("%B %-d, %Y")
-        )
 
 
 class LeaveTeamViewTestCase(SetupUserMixin, TestCase):
