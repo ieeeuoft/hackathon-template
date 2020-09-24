4059ab7154c8b958ad0c5f8a2c9d90b27a753080
Graham Hoyes <accounts@grahamhoyes.com>
2020-09-23 23:05:03 -0400

    Improve saving

--- hackathon_site/review/admin.py
+++ hackathon_site/review/admin.py
@@ -15,11 +15,25 @@ class ReviewForm(forms.ModelForm):
     quality = forms.ChoiceField(choices=int_choices)
     experience = forms.ChoiceField(choices=int_choices)
     status = forms.ChoiceField(choices=[("", "")] + Review.STATUS_CHOICES)
-    reviewer_comments = forms.CharField(widget=forms.Textarea)
+    reviewer_comments = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 3, "cols": 25})
+    )
 
     class Meta:
         model = Application
         fields = "__all__"
+
+    def _save_m2m_and_review(self):
+        """
+        Used to make sure the review instance gets saved if commit=False.
+
+        The standard implementation of .save() is to set
+        ``self.save_m2m = self._save_m2m``, so that the user can call it
+        at a later time. We append ``self._save_m2m`` to also save the review
+        instance.
+        """
+        super()._save_m2m()
+        self.review.save()
 
     def save(self, commit=True):
         """
@@ -47,6 +61,13 @@ class ReviewForm(forms.ModelForm):
 
         if commit:
             review.save()
+        else:
+            self.review = review
+
+            # If commit=False, super().save(commit=False) will set
+            # self.save_m2m = self._save_m2m, for the user to call at a later time.
+            # This will now also save the review instance.
+            self._save_m2m = self._save_m2m_and_review
 
         return super().save(commit)
 
@@ -66,6 +87,10 @@ class ReviewForm(forms.ModelForm):
 
 class ApplicationReviewInlineFormset(forms.BaseInlineFormSet):
     def _construct_form(self, i, **kwargs):
+        """
+        Pass the request down into the form, so we can use it to set the reviewer.
+        ``self.request`` is set by ``ApplicationInline.get_formset()`
+        """
         form = super()._construct_form(i, **kwargs)
         form.request = self.request
         return form
@@ -82,16 +107,17 @@ class ApplicationInline(admin.TabularInline):
     exclude = ("user", "gender", "ethnicity", "phone_number", "resume", "rsvp")
     readonly_fields = (
         "get_user_full_name",
+        "study_level",
+        "school",
+        "graduation_year",
         "q1",
         "q2",
         "q3",
         "get_resume_link",
-        "study_level",
-        "school",
-        "graduation_year",
         "birthday",
         "conduct_agree",
         "data_agree",
+        "get_reviewer_name",
     )
 
     def get_user_full_name(self, obj):
@@ -100,14 +126,24 @@ class ApplicationInline(admin.TabularInline):
     get_user_full_name.short_description = "Full Name"
 
     def get_resume_link(self, obj):
-        link = '<a href="{}" target="_blank">{}</a>'.format(
-            obj.resume.url, obj.resume.name.split("/")[-1]
-        )
+        link = '<a href="{}" target="_blank">Open resume</a>'.format(obj.resume.url)
         return mark_safe(link)
 
     get_resume_link.short_description = "Resume"
 
+    def get_reviewer_name(self, obj):
+        if hasattr(obj, "review") and obj.review.reviewer:
+            return f"{obj.review.reviewer.first_name} {obj.review.reviewer.last_name}"
+        else:
+            return ""
+
+    get_reviewer_name.short_description = "Reviewer name"
+
     def get_formset(self, request, obj=None, **kwargs):
+        """
+        Pass the request into the formset, so that we can then pass it down
+        into the form. Request is used to set the reviewer to the current user.
+        """
         formset = super().get_formset(request, obj, **kwargs)
         formset.request = request
         return formset
