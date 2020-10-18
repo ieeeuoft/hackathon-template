1c9ddf455a8269f6087abc4223654e8247344010
Armin <arminale@yahoo.com>
2020-10-18 13:50:06 -0400

    Changed date widget to date picker for HTML5

--- hackathon_site/review/forms.py
+++ hackathon_site/review/forms.py
@@ -4,8 +4,16 @@ from django import forms
 
 
 class MailerForm(forms.Form):
-    date_start = forms.DateField(label="Start date", initial=datetime.date.today)
-    date_end = forms.DateField(label="End date", initial=datetime.date.today)
+    date_start = forms.DateField(
+        label="Start date",
+        initial=datetime.date.today,
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+    )
+    date_end = forms.DateField(
+        label="End date",
+        initial=datetime.date.today,
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+    )
     status = forms.ChoiceField(
         label="Applicant statuses to send to",
         choices=[
