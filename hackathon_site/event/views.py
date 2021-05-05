bd55e68ab1e87168473bd477ad48b9bd7284555e
AB <stefanalexb@gmail.com>
2021-05-05 14:05:33 -0400

    Small fix to format string

--- hackathon_site/event/views.py
+++ hackathon_site/event/views.py
@@ -108,7 +108,7 @@ class DashboardView(LoginRequiredMixin, FormView):
             )
             context["rsvp_deadline"] = (
                 review.decision_sent_date + timedelta(days=settings.RSVP_DAYS)
-            ).strftime("%B %-d %Y")
+            ).strftime("%B %-d, %Y")
         else:
             context["review"] = None
 
