49b206e1156ec0781fbf876ce018f27842b6c983
Edward Luo <edward.luo@mail.utoronto.ca>
2021-06-05 13:04:59 -0400

    rename endpoint

--- hackathon_site/hardware/api_urls.py
+++ hackathon_site/hardware/api_urls.py
@@ -6,6 +6,6 @@ app_name = "hardware"
 
 urlpatterns = [
     path("hardware/", views.HardwareListView.as_view(), name="hardware-list",),
-    path("orders/", views.HardwareOrderListView.as_view(), name="hardware-order-list",),
+    path("orders/", views.HardwareOrderListView.as_view(), name="order-list",),
     path("categories/", views.CategoryListView.as_view(), name="category-list"),
 ]
