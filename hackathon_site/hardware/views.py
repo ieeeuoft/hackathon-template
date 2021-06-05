f4780277e8d769f9637f25937ad264da687e2e98
Edward Luo <edward.luo@mail.utoronto.ca>
2021-06-05 00:13:58 -0400

    Add List view

--- hackathon_site/hardware/views.py
+++ hackathon_site/hardware/views.py
@@ -1,6 +1,10 @@
-from hardware.models import Hardware, Category
+from hardware.models import Hardware, Category, Order
 from rest_framework import generics, mixins
-from hardware.serializers import HardwareSerializer, CategorySerializer
+from hardware.serializers import (
+    HardwareSerializer,
+    CategorySerializer,
+    HardwareOrderSerializer,
+)
 
 
 class HardwareListView(mixins.ListModelMixin, generics.GenericAPIView):
@@ -14,6 +18,14 @@ class HardwareListView(mixins.ListModelMixin, generics.GenericAPIView):
 class CategoryListView(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class HardwareOrderListView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Order.objects.all()
+    serializer_class = HardwareOrderSerializer
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
