890df9096cf6f2b0087827815e6904742393eb43
Edward Luo <edward.luo@mail.utoronto.ca>
2021-06-06 22:27:11 -0400

    IEEE 17 GET: /api/hardware/orders/ (#279)
    * Add Serializer
    
    * Add List view
    
    * Add API endpoint
    
    * rename endpoint
    
    * fix self.id is not string
    
    * explicitly state serializer fields
    
    * rename endpoint
    
    * Serializer and api tests
    
    * Update test_api.py
    
    add request object to serializer context
    
    * Update test_api.py
    
    add comment
    
    * Rename to Order
    
    * Rename api test
    
    * Rename View

--- hackathon_site/hardware/test_api.py
+++ hackathon_site/hardware/test_api.py
@@ -1,10 +1,14 @@
 from rest_framework.test import APITestCase
-from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 
-from hardware.models import Hardware, Category
-from hardware.serializers import HardwareSerializer, CategorySerializer
+from event.models import Team
+from hardware.models import Hardware, Category, Order, OrderItem
+from hardware.serializers import (
+    HardwareSerializer,
+    CategorySerializer,
+    OrderSerializer,
+)
 from hackathon_site.tests import SetupUserMixin
 
 
@@ -131,3 +135,62 @@ class CategoryListViewTestCase(SetupUserMixin, APITestCase):
         data = response.json()
 
         self.assertEqual(expected_response, data["results"][0])
+
+
+class OrderListViewTestCase(SetupUserMixin, APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.team = Team.objects.create()
+        self.order = Order.objects.create(status="Cart", team=self.team)
+        self.hardware = Hardware.objects.create(
+            name="name",
+            model_number="model",
+            manufacturer="manufacturer",
+            datasheet="/datasheet/location/",
+            notes="notes",
+            quantity_available=4,
+            max_per_team=1,
+            picture="/picture/location",
+        )
+        self.other_hardware = Hardware.objects.create(
+            name="other",
+            model_number="otherModel",
+            manufacturer="otherManufacturer",
+            datasheet="/datasheet/location/other",
+            quantity_available=10,
+            max_per_team=10,
+            picture="/picture/location/other",
+        )
+        OrderItem.objects.create(
+            order=self.order, hardware=self.hardware,
+        )
+        OrderItem.objects.create(
+            order=self.order, hardware=self.other_hardware,
+        )
+        self.order_2 = Order.objects.create(status="Submitted", team=self.team)
+        OrderItem.objects.create(
+            order=self.order_2, hardware=self.hardware,
+        )
+        OrderItem.objects.create(
+            order=self.order_2, hardware=self.other_hardware,
+        )
+        self.view = reverse("api:hardware:order-list")
+
+    def test_user_not_logged_in(self):
+        response = self.client.get(self.view)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_hardware_get_success(self):
+        self._login()
+
+        response = self.client.get(self.view)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        queryset = Order.objects.all()
+        # need to provide a request in the serializer context to produce absolute url for image field
+        expected_response = OrderSerializer(
+            queryset, many=True, context={"request": response.wsgi_request}
+        ).data
+        data = response.json()
+
+        self.assertEqual(expected_response, data["results"])
--- hackathon_site/hardware/views.py
+++ hackathon_site/hardware/views.py
@@ -1,6 +1,10 @@
-from hardware.models import Hardware, Category
+from hardware.models import Hardware, Category, Order
 from rest_framework import generics, mixins
-from hardware.serializers import HardwareSerializer, CategorySerializer
+from hardware.serializers import (
+    HardwareSerializer,
+    CategorySerializer,
+    OrderSerializer,
+)
 
 
 class HardwareListView(mixins.ListModelMixin, generics.GenericAPIView):
@@ -22,6 +26,14 @@ class HardwareDetailView(mixins.RetrieveModelMixin, generics.GenericAPIView):
 class CategoryListView(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class OrderListView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
