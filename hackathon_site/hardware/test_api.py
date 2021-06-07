00ac687245c9864a9ce0ad3949bdfd473b15b929
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2021-06-07 09:45:54 +0800

    IEEE-8 hardware detail (#276)
    * make the detail view api
    
    * unit test for detail view api
    
    * correct the format using black
    
    * correct the format using black #2
    
    * add the test for categories

--- hackathon_site/hardware/test_api.py
+++ hackathon_site/hardware/test_api.py
@@ -51,6 +51,60 @@ class HardwareListViewTestCase(SetupUserMixin, APITestCase):
         self.assertEqual(expected_response, data["results"][0])
 
 
+class HardwareDetailViewTestCase(SetupUserMixin, APITestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.hardware = Hardware.objects.create(
+            name="name",
+            model_number="model",
+            manufacturer="manufacturer",
+            datasheet="/datasheet/location/",
+            quantity_available=4,
+            max_per_team=1,
+            picture="/picture/location",
+        )
+
+        self.hardware_serializer = HardwareSerializer(self.hardware)
+        self.view = reverse(
+            "api:hardware:hardware-detail", kwargs={"pk": self.hardware.id}
+        )
+
+        # Test the categories
+        for item in range(1, 6):
+            category_name = "category" + str(item)
+            self.hardware.categories.add(
+                Category.objects.create(name=category_name, max_per_team=4)
+            )
+
+    def test_user_not_logged_in(self):
+        response = self.client.get(self.view)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_hardware_get_success(self):
+        self._login()
+
+        expected_response = {
+            "id": self.hardware.id,
+            "name": "name",
+            "categories": [category.id for category in self.hardware.categories.all()],
+            "model_number": "model",
+            "manufacturer": "manufacturer",
+            "datasheet": "/datasheet/location/",
+            "quantity_available": 4,
+            "quantity_remaining": 4,
+            "notes": None,
+            "max_per_team": 1,
+            "picture": "http://testserver/media/picture/location",
+        }
+
+        response = self.client.get(self.view)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+
+        self.assertEqual(expected_response, data)
+
+
 class CategoryListViewTestCase(SetupUserMixin, APITestCase):
     def setUp(self):
         super().setUp()
