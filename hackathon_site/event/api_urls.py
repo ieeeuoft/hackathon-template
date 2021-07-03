60c430ae28698b92fd7fc359583c91afcd8a0d59
Arsh Kadakia <53918384+arshinmar@users.noreply.github.com>
2021-07-02 22:45:40 -0400

    IEEE-21 /api/event/teams/team (#280)
    * Updated with full serializer, views and test cases
    
    * Added some updated tests
    
    * Expected final tests
    
    * Updated with truly final tests, pending comment
    
    * Updated with proper message formatting
    
    * Added stuff to gitignore, and formatting changed
    
    * Update .gitignore
    
    * Update views.py
    
    * Update views.py
    
    * Update views.py
    
    * Update tests.py
    
    * Update test_api.py
    
    * Changed profiles related name to profile
    
    * Update serializers.py
    
    * Updated with profile views
    
    * All tests work
    
    * One test still failing despite changed imports
    
    * Removed unused imports

--- hackathon_site/event/api_urls.py
+++ hackathon_site/event/api_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from event import api_views
+from event import api_views, views
 
 app_name = "event"
 
 urlpatterns = [
     path("users/user/", api_views.CurrentUserAPIView.as_view(), name="current-user"),
+    path("teams/team/", views.CurrentTeamAPIView.as_view(), name="current-team"),
 ]
