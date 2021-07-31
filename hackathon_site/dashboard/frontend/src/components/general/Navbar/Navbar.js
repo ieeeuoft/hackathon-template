8421160af237d283fac290e298eb3d9ab01988cd
Edward Luo <edward.luo@mail.utoronto.ca>
2021-07-31 18:52:53 -0400

    remove unused selector

--- hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
+++ hackathon_site/dashboard/frontend/src/components/general/Navbar/Navbar.js
@@ -12,7 +12,7 @@ import { ReactComponent as Inventory } from "assets/images/icons/Hardware.svg";
 // Components
 import Button from "@material-ui/core/Button";
 
-import { logout, logoutSelector } from "slices/users/userSlice";
+import logout from "slices/users/userSlice";
 
 const UnconnectedNavbar = ({ logout, cartQuantity, pathname }) => (
     <nav className={styles.nav}>
