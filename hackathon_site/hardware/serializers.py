f2cda6d839400c770ac51954c5ce1de15fc4b7ca
Leo HC Li <l1040344291@163.com>
2021-08-03 11:55:17 +0800

    remove the comment

--- hackathon_site/hardware/serializers.py
+++ hackathon_site/hardware/serializers.py
@@ -36,7 +36,6 @@ class CategorySerializer(serializers.ModelSerializer):
 
     @staticmethod
     def get_unique_hardware_count(obj: Category) -> int:
-        # return Hardware.objects.filter(categories__id=obj.id).count()
         return obj.hardware_set.annotate(Count("id", distinct=True)).count()
 
 
