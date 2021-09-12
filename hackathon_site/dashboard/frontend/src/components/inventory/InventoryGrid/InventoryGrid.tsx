2d26025ea937835619b89611045607137d82017b
Graham Hoyes <accounts@grahamhoyes.com>
2021-09-12 16:12:01 -0400

    We can use redux hooks now

--- hackathon_site/dashboard/frontend/src/components/inventory/InventoryGrid/InventoryGrid.tsx
+++ hackathon_site/dashboard/frontend/src/components/inventory/InventoryGrid/InventoryGrid.tsx
@@ -3,54 +3,44 @@ import Grid from "@material-ui/core/Grid";
 import styles from "pages/Inventory/Inventory.module.scss";
 import Item from "components/inventory/Item/Item";
 
-import { Hardware } from "api/types";
-import { connect } from "react-redux";
-import { RootState } from "slices/store";
+import { useSelector } from "react-redux";
 import { hardwareSelectors } from "slices/hardware/hardwareSlice";
 import { Typography } from "@material-ui/core";
 
-interface InventoryGridProps {
-    items: Hardware[];
-}
+export const InventoryGrid = () => {
+    const items = useSelector(hardwareSelectors.selectAll);
 
-export const UnconnectedInventoryGrid = ({ items }: InventoryGridProps) => (
-    <Grid direction="row" spacing={2} container>
-        {items.length > 0 ? (
-            items.map((item) => (
-                <Grid
-                    xs={6}
-                    sm={4}
-                    md={3}
-                    lg={2}
-                    xl={1}
-                    className={styles.Item}
-                    key={item.id}
-                    item
-                    // onClick={() => setItemOverviewId(item.id)}
-                    onClick={() => alert(`Clicked ${item.name}`)}
-                >
-                    <Item
-                        image={item.picture}
-                        title={item.name}
-                        total={item.quantity_available}
-                        currentStock={item.quantity_remaining}
-                    />
+    return (
+        <Grid direction="row" spacing={2} container>
+            {items.length > 0 ? (
+                items.map((item) => (
+                    <Grid
+                        xs={6}
+                        sm={4}
+                        md={3}
+                        lg={2}
+                        xl={1}
+                        className={styles.Item}
+                        key={item.id}
+                        item
+                        // onClick={() => setItemOverviewId(item.id)}
+                        onClick={() => alert(`Clicked ${item.name}`)}
+                    >
+                        <Item
+                            image={item.picture}
+                            title={item.name}
+                            total={item.quantity_available}
+                            currentStock={item.quantity_remaining}
+                        />
+                    </Grid>
+                ))
+            ) : (
+                <Grid xs={12} item className={styles.Item}>
+                    <Typography>No items found</Typography>
                 </Grid>
-            ))
-        ) : (
-            <Grid xs={12} item className={styles.Item}>
-                <Typography>No items found</Typography>
-            </Grid>
-        )}
-    </Grid>
-);
+            )}
+        </Grid>
+    );
+};
 
-const mapStateToProps = (state: RootState) => ({
-    items: hardwareSelectors.selectAll(state),
-});
-
-const connector = connect(mapStateToProps, {});
-
-export const ConnectedInventoryGrid = connector(UnconnectedInventoryGrid);
-
-export default ConnectedInventoryGrid;
+export default InventoryGrid;
