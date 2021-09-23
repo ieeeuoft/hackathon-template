6ea7515d5b3302f54dbedd3720b14174fd9f7215
Leo6Leo <leeoo.li@mail.utoronto.ca>
2021-09-23 01:27:59 -0400

    specify the location of permissions.py

--- hackathon_site/event/views.py
+++ hackathon_site/event/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 
-from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.shortcuts import redirect
@@ -8,7 +7,7 @@ from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
-from rest_framework import generics, mixins
+
 from django.conf import settings
 from django_filters import rest_framework as filters
 
@@ -24,7 +23,7 @@ from event.models import Team as EventTeam
 from event.serializers import TeamSerializer
 from event.api_filters import TeamFilter
 
-from permissions import FullDjangoModelPermissions
+from event.permissions import FullDjangoModelPermissions
 
 
 def _now():
