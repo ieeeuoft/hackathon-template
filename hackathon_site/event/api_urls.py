b4362b3c2e65caeccb6c4b411594c0a01b06abf0
Leo6Leo <leeoo.li@mail.utoronto.ca>
2021-09-30 20:36:04 -0400

    reformat the file

--- hackathon_site/event/api_urls.py
+++ hackathon_site/event/api_urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("teams/team/", views.CurrentTeamAPIView.as_view(), name="current-team"),
     path("teams/leave_team/", api_views.LeaveTeamView.as_view(), name="leave-team"),
     path("teams/", views.TeamListView.as_view(), name="team-list"),
-    path("teams/<str:team_code>", views.TeamCodeView.as_view(), name="team-code-detail"),
+    path(
+        "teams/<str:team_code>", views.TeamCodeView.as_view(), name="team-code-detail"
+    ),
 ]
--- hackathon_site/event/views.py
+++ hackathon_site/event/views.py
@@ -234,22 +234,24 @@ class MultipleFieldLookupMixin:
     Apply this mixin to any view or viewset to get multiple field filtering
     based on a `lookup_fields` attribute, instead of the default single field filtering.
     """
-    def get_object(self,  *args, **kwargs):
-        queryset = self.get_queryset()             # Get the base queryset
+
+    def get_object(self, *args, **kwargs):
+        queryset = self.get_queryset()  # Get the base queryset
         queryset = self.filter_queryset(queryset)  # Apply any filter backends
         filter = {}
         for field in self.lookup_fields:
-            if self.kwargs[field]: # Ignore empty fields.
+            if self.kwargs[field]:  # Ignore empty fields.
                 filter[field] = self.kwargs[field]
         obj = get_object_or_404(queryset, **filter)  # Lookup the object
         self.check_object_permissions(self.request, obj)
         return obj
 
+
 class TeamCodeView(MultipleFieldLookupMixin, generics.GenericAPIView):
     queryset = EventTeam.objects.all()
     serializer_class = TeamSerializer
     permission_classes = [FullDjangoModelPermissions]
-    lookup_fields = ['team_code']
+    lookup_fields = ["team_code"]
 
     filter_backends = (filters.DjangoFilterBackend, SearchFilter)
     filterset_class = TeamFilter
@@ -261,7 +263,7 @@ class TeamCodeView(MultipleFieldLookupMixin, generics.GenericAPIView):
     )
 
     def get(self, request, *args, **kwargs):
-        return Response(TeamSerializer(self.get_object(request, *args, **kwargs)).data,status=status.HTTP_200_OK)
-
-
-
+        return Response(
+            TeamSerializer(self.get_object(request, *args, **kwargs)).data,
+            status=status.HTTP_200_OK,
+        )
