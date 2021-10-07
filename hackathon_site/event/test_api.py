51ea7133f3b29549ab6ae836d0c3618c78b1a249
Leo6Leo <leeoo.li@mail.utoronto.ca>
2021-10-07 02:05:03 -0400

    add the url builder

--- hackathon_site/event/test_api.py
+++ hackathon_site/event/test_api.py
@@ -377,6 +377,11 @@ class EventTeamCodeListsViewTestCase(SetupUserMixin, APITestCase):
         super().setUp()
         self.view = reverse("api:event:team-detail", args=[self.team.pk])
 
+    def _build_filter_url(self, **kwargs):
+        return (
+            self.view + "?" + "&".join([f"{key}={val}" for key, val in kwargs.items()])
+        )
+
     def test_team_get_not_login(self):
         response = self.client.get(self.view)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
