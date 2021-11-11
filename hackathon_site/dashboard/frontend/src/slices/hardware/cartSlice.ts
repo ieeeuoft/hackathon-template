2821333cf3f1d30e8feb4fc3a0abff83918a51cc
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2021-11-10 19:17:10 -0500

    added cartSlice and addToCart Reducer

--- hackathon_site/dashboard/frontend/src/slices/hardware/cartSlice.ts
+++ hackathon_site/dashboard/frontend/src/slices/hardware/cartSlice.ts
@@ -1,4 +1,39 @@
+import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+
 export interface CartItem {
     hardware_id: number;
     quantity: number;
 }
+
+interface CartExtraState {
+    isLoading: boolean;
+    error: string | null;
+    next: string | null;
+}
+
+const extraState: CartExtraState = {
+    isLoading: false,
+    error: null,
+    next: null,
+};
+
+const cartAdapter = createEntityAdapter<CartItem>();
+
+export const cartReducerName = "cart";
+export const initialState = cartAdapter.getInitialState(extraState);
+
+const cartSlice = createSlice({
+    name: cartReducerName,
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const payloadQuantity = action.payload.quantity;
+            const payloadHardwareId = action.payload.hardware_id;
+            const dummyCartItem: CartItem = {
+                hardware_id: payloadQuantity,
+                quantity: payloadHardwareId,
+            };
+            cartAdapter.addOne(state, dummyCartItem);
+        },
+    },
+});
