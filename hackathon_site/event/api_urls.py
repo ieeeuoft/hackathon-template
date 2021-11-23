85f9f38329c2bbf1c10e501da6e0dee498789d2a
Arsh Kadakia <53918384+arshinmar@users.noreply.github.com>
2021-11-23 12:22:32 -0500

    IEEE-19 and IEEE-137: PATCH Orders (#312)
    * some initial changes
    
    * some further changes
    
    * Updated with validation method
    
    * Updated with permissions code
    
    * Further changes
    
    * final pre-review changes
    
    * BLACK
    
    * Comprehensive changes (#329)
    
    * BLACK
    
    * Non-test change resolutions
    
    * Fixed any remaining issues
    
    * further implemented feedback
    
    * BLACK
    
    * temp commit
    
    * hopefully final?
    
    * pls be final

--- hackathon_site/event/api_urls.py
+++ hackathon_site/event/api_urls.py
@@ -30,4 +30,9 @@ urlpatterns = [
         name="team-orders",
     ),
     path("teams/<int:pk>/", api_views.TeamDetailView.as_view(), name="team-detail"),
+    path(
+        "teams/team/orders/<int:pk>/",
+        api_views.TeamOrderDetailView.as_view(),
+        name="team-order-detail",
+    ),
 ]
