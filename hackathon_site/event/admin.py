a6fbc319a2733255e75e876bee7807d8c03cc3ac
Graham Hoyes <accounts@grahamhoyes.com>
2021-11-27 15:04:16 -0500

    Add an event team admin

--- hackathon_site/event/admin.py
+++ hackathon_site/event/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
-from .models import Profile, Team as TeamEvent
+from django.db.models import Count
+from event.models import Profile, Team as EventTeam
+
+
+@admin.register(EventTeam)
+class EventTeamAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "team_code",
+        "get_members_count",
+        "created_at",
+    )
+    list_display_links = ("id", "team_code")
+    search_fields = (
+        "id",
+        "team_code",
+    )
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(members_count=Count("profiles", distinct=True))
+        )
+
+    @admin.display(description="Members", ordering="Members")
+    def get_members_count(self, obj):
+        return obj.members_count
+
 
 # Register your models here.
-admin.site.register(TeamEvent)
 admin.site.register(Profile)
