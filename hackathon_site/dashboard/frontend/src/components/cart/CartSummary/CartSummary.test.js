b1fb2962a8cafe8cad3dc6f9af0bf055cf0c4ff2
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2021-12-06 22:19:27 -0500

    updated unit test

--- hackathon_site/dashboard/frontend/src/components/cart/CartSummary/CartSummary.test.js
+++ hackathon_site/dashboard/frontend/src/components/cart/CartSummary/CartSummary.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import CartSummary from "./CartSummary";
 import { cartQuantity } from "testing/mockData";
+import { makeStore } from "slices/store";
+import { Provider } from "react-redux";
 
-it(`Renders correctly when it reads the number ${cartQuantity}`, () => {
-    const { getByText } = render(<CartSummary cartQuantity={cartQuantity} />);
-    expect(getByText(cartQuantity.toString())).toBeInTheDocument();
+it(`Renders correctly when it reads the number ${cartQuantity}`, async () => {
+    const store = makeStore();
+    const { getByText } = render(
+        <Provider store={store}>
+            <CartSummary cartQuantity={cartQuantity} />
+        </Provider>
+    );
+    await waitFor(() => {
+        expect(getByText(cartQuantity.toString())).toBeInTheDocument();
+    });
 });
