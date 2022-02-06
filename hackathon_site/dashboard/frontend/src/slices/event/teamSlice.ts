8f1a3d28969ec45f71be27c184130c9b942a4c6e
YuYing Liang <yuying.liang00@gmail.com>
2022-02-06 18:37:39 -0500

    updated team slice

--- hackathon_site/dashboard/frontend/src/slices/event/teamSlice.ts
+++ hackathon_site/dashboard/frontend/src/slices/event/teamSlice.ts
@@ -4,31 +4,22 @@ import {
     createSelector,
     createSlice,
     PayloadAction,
-    Update,
 } from "@reduxjs/toolkit";
 import { AppDispatch, RootState } from "slices/store";
-import { APIListResponse, Hardware, Team } from "api/types";
-import { get, stripHostnameReturnFilters } from "../../api/api";
-import { displaySnackbar } from "../ui/uiSlice";
-import {
-    getHardwareNextPage,
-    getHardwareWithFilters,
-    hardwareNextSelector,
-    hardwareReducerName,
-} from "../hardware/hardwareSlice";
-import { push } from "connected-react-router";
-import { fetchUserData, userReducerName } from "../users/userSlice";
+import { Team } from "api/types";
+import { get } from "api/api";
+import { displaySnackbar } from "slices/ui/uiSlice";
 
 interface TeamExtraState {
     isLoading: boolean;
     error: string | null;
-    currentTeam: Team;
+    currentTeam: Team | null;
 }
 
 const extraState: TeamExtraState = {
     isLoading: false,
     error: null,
-    currentTeam: NULL,
+    currentTeam: null,
 };
 
 const teamAdapter = createEntityAdapter<Team>({
@@ -39,73 +30,39 @@ export const teamReducerName = "team";
 export const initialState = teamAdapter.getInitialState(extraState);
 export type TeamState = typeof initialState;
 
-export const getCurrentTeam = createAsyncThunk(
-    `${teamReducerName}/getCurrentTeam`,
-    async (arg, { dispatch, rejectWithValue }) => {
-        try {
-            const response = await get("/api/event/teams/team/");
-            return response.data;
-        } catch (e) {
-            if (!e.response) {
-                // This should almost never happen in production, and is likely due to a
-                // network error. Mostly here as a sanity check when running locally
-                dispatch(
-                    displaySnackbar({
-                        message: `Failed to fetch team data: ${e.message}`,
-                        options: { variant: "error" },
-                    })
-                );
-                return rejectWithValue({
-                    status: null,
-                    message: e.message,
-                });
-            } else if (e.response.status === 401) {
-                // Unauthenticated
-                dispatch(push("/login"));
-                return rejectWithValue({
-                    status: 401,
-                    message: e.response.data.detail,
-                });
-            } else {
-                dispatch(
-                    displaySnackbar({
-                        message: `Failed to fetch team data: Error ${e.response.status}`,
-                        options: { variant: "error" },
-                    })
-                );
-                return rejectWithValue({
-                    status: e.response.status,
-                    message: e.response.data,
-                });
-            }
-        }
+// Thunks
+interface RejectValue {
+    status: number;
+    message: any;
+}
+
+export const getCurrentTeam = createAsyncThunk<
+    Team,
+    void,
+    { state: RootState; rejectValue: RejectValue; dispatch: AppDispatch }
+>(`${teamReducerName}/getCurrentTeam`, async (arg, { dispatch, rejectWithValue }) => {
+    try {
+        const response = await get<Team>("/api/event/teams/team/");
+        return response.data;
+    } catch (e: any) {
+        dispatch(
+            displaySnackbar({
+                message: `Failed to fetch team data: Error ${e.response.status}`,
+                options: { variant: "error" },
+            })
+        );
+        return rejectWithValue({
+            status: e.response.status,
+            message: e.response.data,
+        });
     }
-);
+});
 
 // Slice
 const teamSlice = createSlice({
     name: teamReducerName,
     initialState,
-    reducers: {
-        // addToCart: (state, { payload }: PayloadAction<Team>) => {
-        //     const existingEntity = state.entities[payload.hardware_id.toString()];
-        //     if (existingEntity) {
-        //         cartAdapter.upsertOne(state, {
-        //             hardware_id: payload.hardware_id,
-        //             quantity: existingEntity.quantity + payload.quantity,
-        //         });
-        //     } else {
-        //         cartAdapter.addOne(state, payload);
-        //     }
-        // },
-        // removeFromCart: (state, { payload }: PayloadAction<number>) => {
-        //     cartAdapter.removeOne(state, payload);
-        // },
-        // updateCart: (state, { payload }: PayloadAction<Update<CartItem>>) => {
-        //     cartAdapter.updateOne(state, payload);
-        // },
-    },
-
+    reducers: {},
     extraReducers: (builder) => {
         builder.addCase(getCurrentTeam.pending, (state) => {
             state.isLoading = true;
@@ -123,18 +80,7 @@ const teamSlice = createSlice({
             }
         );
 
-        // builder.addCase(getCurrentTeam.fulfilled, (state, { payload }) => {
-        //     state.isLoading = false;
-        //     state.error = null;
-        //     if (payload) {
-        //         state.next = payload.next;
-        //         state.count = payload.count;
-        //         hardwareAdapter.addMany(state, payload.results);
-        //     }
-        // });
-
         builder.addCase(getCurrentTeam.rejected, (state, { payload }) => {
-            state.isMoreLoading = false;
             state.error = payload?.message || "Something went wrong";
         });
     },
@@ -142,7 +88,6 @@ const teamSlice = createSlice({
 
 export const { actions, reducer } = teamSlice;
 export default reducer;
-export const { addToTeam, removeFromTeam, updateTeam } = actions;
 
 // Selectors
 export const teamSliceSelector = (state: RootState) => state[teamReducerName];
