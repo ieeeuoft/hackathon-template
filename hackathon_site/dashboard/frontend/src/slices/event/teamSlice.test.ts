fb6502c8d2fa72765e9dd70886945a39be8e5822
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2022-02-26 12:36:51 -0500

    create the unit test for getCurrentTeam thunk

--- hackathon_site/dashboard/frontend/src/slices/event/teamSlice.test.ts
+++ hackathon_site/dashboard/frontend/src/slices/event/teamSlice.test.ts
@@ -1,131 +1,51 @@
 import store, { makeStore, RootState } from "slices/store";
 import {
-    addToCart,
-    cartReducerName,
-    cartSelectors,
-    cartSliceSelector,
+    teamReducerName,
+    teamSelectors,
+    teamSliceSelector,
     isLoadingSelector,
     initialState,
-    removeFromCart,
-    updateCart,
-} from "slices/hardware/cartSlice";
+    getCurrentTeam,
+} from "slices/event/teamSlice";
 import { makeStoreWithEntities, waitFor } from "testing/utils";
-import { mockCartItems } from "testing/mockData";
+import { mockTeam } from "testing/mockData";
 
 const mockState: RootState = {
     ...store.getState(),
-    [cartReducerName]: initialState,
+    [teamReducerName]: initialState,
 };
 
 describe("Selectors", () => {
-    test("cartSliceSelector returns the cart store", () => {
-        expect(cartSliceSelector(mockState)).toEqual(mockState[cartReducerName]);
+    test("teamSliceSelector returns the team store", () => {
+        expect(teamSliceSelector(mockState)).toEqual(mockState[teamReducerName]);
     });
 
     test("isLoadingSelector", () => {
         const loadingTrueState = {
             ...mockState,
-            [cartReducerName]: {
+            [teamReducerName]: {
                 ...initialState,
                 isLoading: true,
             },
         };
         const loadingFalseState = {
             ...mockState,
-            [cartReducerName]: {
+            [teamReducerName]: {
                 ...initialState,
                 isLoading: false,
             },
         };
-
         expect(isLoadingSelector(loadingTrueState)).toEqual(true);
         expect(isLoadingSelector(loadingFalseState)).toEqual(false);
     });
 });
 
-describe("addToCart action", () => {
-    test("addToCart with new items", async () => {
+describe("getCurrentTeam action", () => {
+    test("get the detail info about the current logged in team", async () => {
         const store = makeStore();
-
-        store.dispatch(addToCart({ hardware_id: 1, quantity: 2 }));
-
-        store.dispatch(addToCart({ hardware_id: 2, quantity: 3 }));
-
+        await store.dispatch(getCurrentTeam());
         await waitFor(() => {
-            expect(cartSelectors.selectIds(store.getState())).toEqual([1, 2]);
-            expect(cartSelectors.selectById(store.getState(), 1)).toEqual({
-                hardware_id: 1,
-                quantity: 2,
-            });
-            expect(cartSelectors.selectById(store.getState(), 2)).toEqual({
-                hardware_id: 2,
-                quantity: 3,
-            });
-        });
-    });
-
-    test("addToCart updates existing item", async () => {
-        const store = makeStore();
-
-        store.dispatch(addToCart({ hardware_id: 1, quantity: 2 }));
-        store.dispatch(addToCart({ hardware_id: 2, quantity: 3 }));
-        store.dispatch(addToCart({ hardware_id: 1, quantity: 3 }));
-
-        await waitFor(() => {
-            expect(cartSelectors.selectIds(store.getState())).toEqual([1, 2]);
-            expect(cartSelectors.selectById(store.getState(), 1)).toEqual({
-                hardware_id: 1,
-                quantity: 5,
-            });
-            expect(cartSelectors.selectById(store.getState(), 2)).toEqual({
-                hardware_id: 2,
-                quantity: 3,
-            });
-        });
-    });
-});
-
-describe("removeFromCart action", () => {
-    test("removeFromCart removes existing item", async () => {
-        const store = makeStoreWithEntities({
-            cartItems: mockCartItems,
-        });
-
-        store.dispatch(removeFromCart(2));
-
-        await waitFor(() => {
-            expect(cartSelectors.selectAll(store.getState()).length).toEqual(2);
-            expect(cartSelectors.selectById(store.getState(), 1)).toEqual({
-                hardware_id: 1,
-                quantity: 3,
-            });
-            expect(cartSelectors.selectById(store.getState(), 3)).toEqual({
-                hardware_id: 3,
-                quantity: 2,
-            });
-            expect(cartSelectors.selectById(store.getState(), 2)).toEqual(undefined);
-        });
-    });
-});
-
-describe("updateCart action", () => {
-    test("updateCart updates quantity of existing item", async () => {
-        const store = makeStoreWithEntities({
-            cartItems: mockCartItems,
-        });
-
-        store.dispatch(
-            updateCart({ id: mockCartItems[0].hardware_id, changes: { quantity: 25 } })
-        );
-
-        await waitFor(() => {
-            expect(cartSelectors.selectAll(store.getState()).length).toEqual(
-                mockCartItems.length
-            );
-            expect(cartSelectors.selectById(store.getState(), 1)).toEqual({
-                hardware_id: mockCartItems[0].hardware_id,
-                quantity: 25,
-            });
+            expect(teamSelectors.selectById(store.getState(), 1)).toEqual(mockTeam);
         });
     });
 });
