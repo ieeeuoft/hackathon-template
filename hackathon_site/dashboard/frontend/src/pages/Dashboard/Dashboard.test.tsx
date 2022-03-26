1a35454d529f4942c6d8f63b5a9e2fe84c907060
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2022-03-26 12:24:20 -0400

    updated some tests

--- hackathon_site/dashboard/frontend/src/pages/Dashboard/Dashboard.test.tsx
+++ hackathon_site/dashboard/frontend/src/pages/Dashboard/Dashboard.test.tsx
@@ -32,9 +32,9 @@ const hardwareUri = "/api/hardware/hardware/";
 const categoriesUri = "/api/hardware/categories/";
 const teamUri = "/api/event/teams/team/";
 
-it("Renders correctly when the dashboard appears 4 cards and 3 tables", () => {
-    const { getByText, queryByText } = render(<Dashboard />);
-    waitFor(() => {
+describe("Dashboard Page", () => {
+    it("Renders correctly when the dashboard appears 4 cards and 3 tables", () => {
+        const { queryByText, getByText } = render(<Dashboard />);
         for (let e of cardItems) {
             expect(queryByText(e.title)).toBeTruthy();
         }
@@ -42,50 +42,53 @@ it("Renders correctly when the dashboard appears 4 cards and 3 tables", () => {
         expect(getByText("Pending Orders")).toBeInTheDocument();
         // TODO: add check for returned items and broken items when those ar
     });
-});
+    it("Opens Product Overview with the correct hardware information", async () => {
+        const hardwareApiResponse = makeMockApiListResponse(mockHardware);
+        const categoryApiResponse = makeMockApiListResponse(mockCategories);
 
-it("Opens Product Overview with the correct hardware information", async () => {
-    const hardwareApiResponse = makeMockApiListResponse(mockHardware);
-    const categoryApiResponse = makeMockApiListResponse(mockCategories);
+        const allOrders = mockPendingOrders.concat(mockCheckedOutOrders);
+        const hardware_ids = allOrders.flatMap((order) =>
+            order.items.map((item) => item.hardware_id)
+        );
+        when(mockedGet)
+            .calledWith(hardwareUri, { hardware_ids })
+            .mockResolvedValue(hardwareApiResponse);
+        when(mockedGet)
+            .calledWith(categoriesUri, {})
+            .mockResolvedValue(categoryApiResponse);
 
-    const allOrders = mockPendingOrders.concat(mockCheckedOutOrders);
-    const hardware_ids = allOrders.flatMap((order) =>
-        order.items.map((item) => item.hardware_id)
-    );
-    when(mockedGet)
-        .calledWith(hardwareUri, { hardware_ids })
-        .mockResolvedValue(hardwareApiResponse);
-    when(mockedGet).calledWith(categoriesUri).mockResolvedValue(categoryApiResponse);
+        const { getByTestId, getByText } = render(<Dashboard />);
 
-    const { getByTestId, getByText } = render(<Dashboard />);
+        const hardware = mockHardware.find(
+            ({ id }) => id === mockCheckedOutOrders[0].items[0].hardware_id
+        );
+        const category = mockCategories.find(
+            ({ id }) => id === hardware?.categories[0]
+        );
 
-    const hardware = mockHardware.find(
-        ({ id }) => id === mockCheckedOutOrders[0].items[0].hardware_id
-    );
-    const category = mockCategories.find(({ id }) => id === hardware?.categories[0]);
-
-    if (hardware && category) {
-        await waitFor(() => {
-            const infoButton = within(
-                getByTestId(
-                    `checked-out-hardware-${hardware.id}-${mockCheckedOutOrders[0].id}`
-                )
-            ).getByTestId("info-button");
-            fireEvent.click(infoButton);
-            expect(getByText("Product Overview")).toBeVisible();
-            expect(
-                getByText(`- Max ${hardware.max_per_team} of this item`)
-            ).toBeInTheDocument();
-            expect(
-                getByText(
-                    `- Max ${mockCategories[0].max_per_team} of items under category ${mockCategories[0].name}`
-                )
-            ).toBeInTheDocument();
-            expect(getByText(hardware.model_number)).toBeInTheDocument();
-            expect(getByText(hardware.manufacturer)).toBeInTheDocument();
-            expect(getByText(hardware.notes)).toBeInTheDocument();
-        });
-    }
+        if (hardware && category) {
+            await waitFor(() => {
+                const infoButton = within(
+                    getByTestId(
+                        `checked-out-hardware-${hardware.id}-${mockCheckedOutOrders[0].id}`
+                    )
+                ).getByTestId("info-button");
+                fireEvent.click(infoButton);
+                expect(getByText("Product Overview")).toBeVisible();
+                expect(
+                    getByText(`- Max ${hardware.max_per_team} of this item`)
+                ).toBeInTheDocument();
+                expect(
+                    getByText(
+                        `- Max ${mockCategories[0].max_per_team} of items under category ${mockCategories[0].name}`
+                    )
+                ).toBeInTheDocument();
+                expect(getByText(hardware.model_number)).toBeInTheDocument();
+                expect(getByText(hardware.manufacturer)).toBeInTheDocument();
+                expect(getByText(hardware.notes)).toBeInTheDocument();
+            });
+        }
+    });
 });
 
 it("get info on the current team", async () => {
