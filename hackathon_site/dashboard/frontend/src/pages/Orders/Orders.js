352a171c0bab50ca02ad0e42fac6a03e0a0b61d9
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2022-05-22 23:44:34 -0400

    add the prototype of EditTeamModal

--- hackathon_site/dashboard/frontend/src/pages/Orders/Orders.js
+++ hackathon_site/dashboard/frontend/src/pages/Orders/Orders.js
@@ -2,13 +2,14 @@ import React from "react";
 // import styles from "./Orders.module.scss";
 import Header from "components/general/Header/Header";
 import Typography from "@material-ui/core/Typography";
-
+import TeamModal from "../../components/dashboard/EditTeamModal/TeamModal";
 const Orders = () => {
     return (
         <div>
             <Header />
             <Typography variant="h1">Orders</Typography>
-            <p>IEEEEEE</p>
+            <p>IEEEEEsdfsdfsdE</p>
+            <TeamModal></TeamModal>
         </div>
     );
 };
