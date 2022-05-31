fa2411df69422081f44c704d69dd06b2e298c4db
ethanhugh <ethanhugh88@gmail.com>
2022-05-31 10:44:55 -0400

    Added an alert in index.tsx that gives the user a warning if their team size is invald, displaying a different message depending on whether the team is too large or too small.

--- hackathon_site/dashboard/frontend/src/components/cart/CartErrorBox/index.tsx
+++ hackathon_site/dashboard/frontend/src/components/cart/CartErrorBox/index.tsx
@@ -3,10 +3,13 @@ import { useSelector } from "react-redux";
 import { cartTotalSelector, errorSelector } from "slices/hardware/cartSlice";
 import Grid from "@material-ui/core/Grid";
 import { Alert, AlertTitle } from "@material-ui/lab";
+import { teamSizeSelector } from "../../../slices/event/teamSlice";
+import { maxTeamSize, minTeamSize } from "../../../constants";
 
 const CartErrorBox = () => {
     const cartQuantity = useSelector(cartTotalSelector);
     const orderSubmissionError = useSelector(errorSelector);
+    const teamSize = useSelector(teamSizeSelector);
 
     return (
         <Grid direction="row" container>
@@ -28,6 +31,18 @@ const CartErrorBox = () => {
                         ) : (
                             orderSubmissionError
                         )}
+                    </Alert>
+                )}
+                {teamSize < minTeamSize && (
+                    <Alert severity="warning" style={{ margin: "15px 0px" }}>
+                        <AlertTitle>{`Team Size Too Small!`}</AlertTitle>
+                        Sorry, you need to add more team members to place an order.
+                    </Alert>
+                )}
+                {teamSize > maxTeamSize && (
+                    <Alert severity="warning" style={{ margin: "15px 0px" }}>
+                        <AlertTitle>{`Team Size Too Large!`}</AlertTitle>
+                        Sorry, you have too many team members to place an order.
                     </Alert>
                 )}
             </Grid>
