b4b785566ef2fbfa216208cc0b9963acb44b0f2f
lukecheseldine <cheeseldine@gmail.com>
2022-06-10 19:09:14 -0400

    created files, moved some code into them

---
+++ hackathon_site/dashboard/frontend/src/components/teamDetail/TeamInfoTable/TeamInfoTable.tsx
@@ -0,0 +1,107 @@
+import { RouteComponentProps } from "react-router-dom";
+import Header from "components/general/Header/Header";
+import {
+    Checkbox,
+    Container,
+    Grid,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { mockTeamMultiple } from "testing/mockData";
+import React from "react";
+import { PageParams } from "pages/TeamDetail/TeamDetail";
+
+export const TeamDetail = ({ match }: RouteComponentProps<PageParams>) => {
+    return (
+        <>
+            <Container>
+                <Grid container direction="column" spacing={6}>
+                    <Grid item>
+                        <Typography variant="h1">
+                            Team {match.params.id} Overview
+                        </Typography>
+                    </Grid>
+                    {/* Team info and actions */}
+                    <Grid container spacing={2}>
+                        <Grid
+                            container
+                            direction="column"
+                            spacing={1}
+                            item
+                            md={6}
+                            xs={12}
+                        >
+                            <Grid item>
+                                <Typography variant="h2">Team info</Typography>
+                            </Grid>
+                            <Grid item>
+                                <TableContainer component={Paper}>
+                                    <Table size="small">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Name</TableCell>
+                                                <TableCell>Email</TableCell>
+                                                <TableCell>Phone</TableCell>
+                                                <TableCell>ID</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {mockTeamMultiple.profiles.map((row) => (
+                                                <TableRow key={row.user.id}>
+                                                    <TableCell>
+                                                        {`${row.user.first_name} ${row.user.last_name}`}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {row.user.email}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {row.user.phone}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Checkbox
+                                                            checked={row.id_provided}
+                                                            color="primary"
+                                                            style={{
+                                                                marginLeft: "-15px",
+                                                            }}
+                                                        />
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Grid>
+                        </Grid>
+
+                        <Grid
+                            container
+                            spacing={1}
+                            direction="column"
+                            item
+                            md={6}
+                            xs={12}
+                        >
+                            <Grid item>
+                                <Typography variant="h2">Dummy</Typography>
+                            </Grid>
+                            <Grid item>
+                                <TableContainer component={Paper}>
+                                    <Table>Dummy</Table>
+                                </TableContainer>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
+    );
+};
+
+export default TeamDetail;
--- hackathon_site/dashboard/frontend/src/pages/TeamDetail/TeamDetail.tsx
+++ hackathon_site/dashboard/frontend/src/pages/TeamDetail/TeamDetail.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "components/general/Header/Header";
 import Typography from "@material-ui/core/Typography";
+import TeamInfoTable from "components/teamDetail/TeamInfoTable/TeamInfoTable";
 import { mockTeamMultiple } from "testing/mockData";
 import {
     Table,
@@ -20,92 +21,11 @@ export interface PageParams {
     id: string;
 }
 
-const TeamDetail = ({ match }: RouteComponentProps<PageParams>) => {
+const TeamDetail = () => {
     return (
         <>
             <Header />
-            <Container>
-                <Grid container direction="column" spacing={6}>
-                    <Grid item>
-                        <Typography variant="h1">
-                            Team {match.params.id} Overview
-                        </Typography>
-                    </Grid>
-                    {/* Team info and actions */}
-                    <Grid container spacing={2}>
-                        <Grid
-                            container
-                            direction="column"
-                            spacing={1}
-                            item
-                            md={6}
-                            xs={12}
-                        >
-                            <Grid item>
-                                <Typography variant="h2">Team info</Typography>
-                            </Grid>
-                            <Grid item>
-                                <TableContainer component={Paper}>
-                                    <Table size="small">
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>Name</TableCell>
-                                                <TableCell>Email</TableCell>
-                                                <TableCell>Phone</TableCell>
-                                                <TableCell>ID</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {mockTeamMultiple.profiles.map((row) => (
-                                                <TableRow key={row.user.id}>
-                                                    <TableCell>
-                                                        {`${row.user.first_name} ${row.user.last_name}`}
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        {row.user.email}
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        {row.user.phone}
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Checkbox
-                                                            checked={row.id_provided}
-                                                            color="primary"
-                                                            style={{
-                                                                marginLeft: "-15px",
-                                                            }}
-                                                        />
-                                                    </TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            </Grid>
-                        </Grid>
-
-                        <Grid
-                            container
-                            spacing={1}
-                            direction="column"
-                            item
-                            md={6}
-                            xs={12}
-                        >
-                            <Grid item>
-                                <Typography variant="h2">Dummy</Typography>
-                            </Grid>
-                            <Grid item>
-                                <TableContainer component={Paper}>
-                                    <Table>Dummy</Table>
-                                </TableContainer>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Container>
+            <TeamInfoTable match={id} />
         </>
     );
 };
-
-export default TeamDetail;
