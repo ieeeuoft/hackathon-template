7f398dd0144778159cc72d94c3da4c058846994a
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2022-06-16 00:07:53 -0400

    revert the order.js file

--- hackathon_site/dashboard/frontend/src/pages/Orders/Orders.js
+++ hackathon_site/dashboard/frontend/src/pages/Orders/Orders.js
@@ -2,14 +2,13 @@ import React from "react";
 // import styles from "./Orders.module.scss";
 import Header from "components/general/Header/Header";
 import Typography from "@material-ui/core/Typography";
-import EditTeamModal from "../../components/dashboard/EditTeamModal/EditTeamModal";
+
 const Orders = () => {
     return (
         <>
             <Header />
             <Typography variant="h1">Orders</Typography>
             <p>IEEEEEE</p>
-            {/*<EditTeamModal teamCode={"abced"} canChangeTeam={true} />*/}
         </>
     );
 };
