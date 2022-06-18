0c47823c24d5c4371284d3edd646917a323df2f0
Karandeep Lubana <karandeep.lubana@utoronto.ca>
2022-06-18 17:01:56 -0400

    IEEE-202 update Orders.test.tsx

--- hackathon_site/dashboard/frontend/src/pages/Orders/Orders.test.tsx
+++ hackathon_site/dashboard/frontend/src/pages/Orders/Orders.test.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+import {
+    makeMockApiListResponse,
+    waitFor,
+    when,
+    fireEvent,
+    promiseResolveWithDelay,
+    makeMockApiResponse,
+} from "testing/utils";
+
 import { render } from "testing/utils";
 
 import Orders from "./Orders";
 
 test("renders without crashing", () => {
     const { getByText } = render(<Orders />);
-    expect(getByText("IEEEEEE")).toBeInTheDocument();
+    expect(getByText("Orders")).toBeInTheDocument();
 });
