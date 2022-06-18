b574c188f9403200d151667b80badfd5e716af6c
Karandeep Lubana <karandeep.lubana@utoronto.ca>
2022-06-17 20:28:21 -0400

    IEEE-202 implement OrdersSearch and Orders components

---
+++ hackathon_site/dashboard/frontend/src/components/Orders/OrdersSearch/OrdersSearch.tsx
@@ -0,0 +1,53 @@
+import { Box, IconButton, InputAdornment, TextField } from "@material-ui/core";
+import { FormikValues } from "formik";
+import React from "react";
+import CloseIcon from "@material-ui/icons/Close";
+import SearchIcon from "@material-ui/icons/Search";
+import styles from "pages/Orders/Orders.module.scss";
+
+const OrdersSearch = ({
+    handleChange,
+    handleReset,
+    handleSubmit,
+}: // values: { search },
+FormikValues) => {
+    return (
+        <form onReset={handleReset} onSubmit={handleSubmit} autoComplete="off">
+            <Box display="flex" flexDirection="row">
+                <TextField
+                    className={styles.ordersBodyToolbarSearch}
+                    id="search-input"
+                    name="search"
+                    label="Search items"
+                    variant="outlined"
+                    type="text"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={handleReset}
+                                    data-testid="clear-button"
+                                >
+                                    <CloseIcon />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                    // value={search}
+                    onChange={handleChange}
+                />
+                <IconButton
+                    color="primary"
+                    aria-label="Search"
+                    onClick={handleSubmit}
+                    data-testid="search-button"
+                    className={styles.ordersBodyToolbarIconButton}
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Box>
+        </form>
+    );
+};
+
+export default OrdersSearch;
--- hackathon_site/dashboard/frontend/src/pages/Orders/Orders.tsx
+++ hackathon_site/dashboard/frontend/src/pages/Orders/Orders.tsx
@@ -1,14 +1,59 @@
 import React from "react";
-// import styles from "./Orders.module.scss";
 import Header from "components/general/Header/Header";
 import Typography from "@material-ui/core/Typography";
+import OrdersSearch from "components/Orders/OrdersSearch/OrdersSearch";
+import { Divider, Grid, IconButton } from "@material-ui/core";
+import styles from "./Orders.module.scss";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import RefreshIcon from "@material-ui/icons/Refresh";
 
 const Orders = () => {
     return (
         <>
             <Header />
-            <Typography variant="h1">Orders</Typography>
-            <p>IEEEEEE</p>
+            <div className={styles.orders}>
+                <Grid container spacing={2} className={styles.ordersBody}>
+                    <Grid item md={8} xl={10} xs={10} className={styles.ordersBody}>
+                        <Typography variant="h1" className={styles.ordersBodyTitle}>
+                            Orders
+                        </Typography>
+                        <div className={styles.ordersBodyToolbar}>
+                            <div>
+                                <OrdersSearch />
+                            </div>
+
+                            <Divider
+                                orientation="vertical"
+                                className={styles.ordersBodyToolbarDivider}
+                                flexItem
+                            />
+
+                            <div className={styles.ordersBodyToolbarDiv}>
+                                <IconButton
+                                    color="primary"
+                                    aria-label="Search"
+                                    // onClick={handleSubmit}
+                                    data-testid="search-button"
+                                >
+                                    <FilterListIcon color="primary" />
+                                </IconButton>
+
+                                <div className={styles.ordersBodyToolbarRefresh}>
+                                    <Typography variant="body2">2 results</Typography>
+                                    <IconButton
+                                        color="primary"
+                                        aria-label="Refresh"
+                                        // onClick={refreshHardware}
+                                        data-testid="refreshInventory"
+                                    >
+                                        <RefreshIcon />
+                                    </IconButton>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
         </>
     );
 };
