bdb8b7f0fdfbb3053df778d7f50b93db100794af
YuYing_Liang <yuying.liang00@gmail.com>
2022-06-26 19:26:04 -0400

    IEEE-193: pending order table formatting fixes (#409)
    * IEEE-193: fixed display problems
    
    * generalized table jsx and styling

--- hackathon_site/dashboard/frontend/src/components/teamDetail/TeamPendingOrderTable/TeamPendingOrderTable.tsx
+++ hackathon_site/dashboard/frontend/src/components/teamDetail/TeamPendingOrderTable/TeamPendingOrderTable.tsx
@@ -2,6 +2,7 @@ import {
     Button,
     Checkbox,
     Grid,
+    Link,
     Paper,
     Table,
     TableBody,
@@ -14,12 +15,15 @@ import Typography from "@material-ui/core/Typography";
 import { mockPendingOrders, mockHardware } from "testing/mockData";
 import React from "react";
 import Container from "@material-ui/core/Container";
-import styles from "components/dashboard/ItemTable/ItemTable.module.scss";
+import styles from "components/general/OrderTables/OrderTables.module.scss";
 import hardwareImagePlaceholder from "assets/images/placeholders/no-hardware-image.svg";
 import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import FormControl from "@material-ui/core/FormControl";
+import {
+    GeneralOrderTableTitle,
+    GeneralOrderTitle,
+} from "components/general/OrderTables/OrderTables";
 
 const createDropdownList = (number: number) => {
     let entry = [];
@@ -73,98 +77,57 @@ export const TeamPendingOrderTable = () => {
     const testfunc = () => {};
 
     return (
-        <Container maxWidth={false} disableGutters={true}>
-            {orders.map((pendingOrder) => (
-                <Grid container direction="column" spacing={1} item md={6} xs={12}>
-                    <Grid item>
-                        <Typography variant="h2">{`Order #${pendingOrder.id}`}</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Table className={styles.table}>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell className={styles.width1} />
-                                    <TableCell className={styles.width2}>
-                                        Name
-                                    </TableCell>
-                                    <TableCell className={`${styles.width1}`}>
-                                        Model
-                                    </TableCell>
-                                    <TableCell className={`${styles.width1}`}>
-                                        Manufacturer
-                                    </TableCell>
-                                    <TableCell className={`${styles.width1}`}>
-                                        Qty requested
-                                    </TableCell>
-                                    <TableCell
-                                        className={`${styles.width1} ${styles.noWrap}`}
-                                    >
-                                        Qty granted
-                                    </TableCell>
-                                    <TableCell className={`${styles.width1}`}>
-                                        Time
-                                    </TableCell>
-                                    <TableCell>
-                                        <Checkbox
-                                            color="primary"
-                                            style={{
-                                                marginLeft: "-15px",
-                                            }}
-                                        />
-                                    </TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {pendingOrder.request.map((row) => (
-                                    <TableRow key={row.id}>
-                                        <TableCell>
-                                            <img
-                                                className={styles.itemImg}
-                                                src={
-                                                    hardware[row.id]?.picture ??
-                                                    hardwareImagePlaceholder
-                                                }
-                                                alt={hardware[row.id]?.name}
-                                            />
-                                        </TableCell>
-                                        <TableCell>
-                                            {`${hardware[row.id]?.name}`}
-                                        </TableCell>
-                                        <TableCell>
-                                            {`${hardware[row.id]?.model_number}`}
-                                        </TableCell>
-                                        <TableCell>
-                                            {`${hardware[row.id]?.manufacturer}`}
-                                        </TableCell>
-                                        <TableCell>
-                                            {`${row.requested_quantity}`}
+        <Container
+            className={styles.tableContainer}
+            maxWidth={false}
+            disableGutters={true}
+        >
+            {orders.length > 0 && (
+                <GeneralOrderTitle
+                    title="Requested Items"
+                    isVisible={true}
+                    toggleVisibility={() => alert("show/hide orders")}
+                />
+            )}
+            {orders.length &&
+                orders.map((pendingOrder) => (
+                    <div key={pendingOrder.id}>
+                        <GeneralOrderTableTitle
+                            orderId={pendingOrder.id}
+                            orderStatus={pendingOrder.status}
+                        />
+                        <TableContainer component={Paper} elevation={2} square={true}>
+                            <Table className={styles.table} size="small">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell className={styles.widthFixed} />
+                                        <TableCell className={styles.width6}>
+                                            Name
                                         </TableCell>
                                         <TableCell
-                                            style={{
-                                                display: "inline-block",
-                                                verticalAlign: "baseline",
-                                            }}
-                                            className={`${styles.width2} ${styles.noWrap}`}
+                                            className={`${styles.width1} ${styles.noWrap}`}
                                         >
-                                            All
-                                            <FormControl
-                                                variant="standard"
-                                                hiddenLabel={true}
-                                            >
-                                                <Select
-                                                    value={0}
-                                                    label="Qty"
-                                                    labelId="qtyLabel"
-                                                    name="quantity"
-                                                >
-                                                    {createDropdownList(
-                                                        row.requested_quantity
-                                                    )}
-                                                </Select>
-                                            </FormControl>
+                                            Model
                                         </TableCell>
-                                        <TableCell>
-                                            {convertToDateTime(pendingOrder.created_at)}
+                                        <TableCell
+                                            className={`${styles.width1} ${styles.noWrap}`}
+                                        >
+                                            Manufacturer
+                                        </TableCell>
+                                        <TableCell
+                                            className={`${styles.width1} ${styles.noWrap}`}
+                                        >
+                                            Qty requested
+                                        </TableCell>
+                                        <TableCell
+                                            className={`${styles.width6} ${styles.noWrap}`}
+                                        >
+                                            Qty granted
+                                        </TableCell>
+                                        <TableCell
+                                            className={`${styles.width1} ${styles.noWrap}`}
+                                        >
+                                            Time
                                         </TableCell>
                                         <TableCell>
                                             <Checkbox
@@ -175,20 +138,108 @@ export const TeamPendingOrderTable = () => {
                                             />
                                         </TableCell>
                                     </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </Grid>
-                    <Grid container justifyContent="flex-end">
-                        <Button color="secondary" variant="text" disableElevation>
-                            Reject Order
-                        </Button>
-                        <Button color="primary" variant="contained" disableElevation>
-                            Complete Order
-                        </Button>
-                    </Grid>
-                </Grid>
-            ))}
+                                </TableHead>
+                                <TableBody>
+                                    {pendingOrder.request.map((row) => (
+                                        <TableRow key={row.id}>
+                                            <TableCell>
+                                                <img
+                                                    className={styles.itemImg}
+                                                    src={
+                                                        hardware[row.id]?.picture ??
+                                                        hardwareImagePlaceholder
+                                                    }
+                                                    alt={hardware[row.id]?.name}
+                                                />
+                                            </TableCell>
+                                            <TableCell>
+                                                {hardware[row.id]?.name}
+                                            </TableCell>
+                                            <TableCell>
+                                                {hardware[row.id]?.model_number}
+                                            </TableCell>
+                                            <TableCell>
+                                                {hardware[row.id]?.manufacturer}
+                                            </TableCell>
+                                            <TableCell>
+                                                {row.requested_quantity}
+                                            </TableCell>
+                                            <TableCell>
+                                                <div
+                                                    style={{
+                                                        display: "flex",
+                                                        alignItems: "end",
+                                                    }}
+                                                >
+                                                    <Link
+                                                        underline="always"
+                                                        color="textPrimary"
+                                                        style={{ marginRight: "15px" }}
+                                                    >
+                                                        All
+                                                    </Link>
+                                                    <FormControl
+                                                        variant="standard"
+                                                        hiddenLabel={true}
+                                                    >
+                                                        <Select
+                                                            value={0}
+                                                            label="Qty"
+                                                            labelId="qtyLabel"
+                                                            name="quantity"
+                                                        >
+                                                            {createDropdownList(
+                                                                row.requested_quantity
+                                                            )}
+                                                        </Select>
+                                                    </FormControl>
+                                                </div>
+                                            </TableCell>
+                                            <TableCell>
+                                                {convertToDateTime(
+                                                    pendingOrder.created_at
+                                                )}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Checkbox
+                                                    color="primary"
+                                                    style={{
+                                                        marginLeft: "-15px",
+                                                    }}
+                                                />
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <Grid
+                            container
+                            justifyContent="flex-end"
+                            spacing={1}
+                            style={{ marginTop: "10px" }}
+                        >
+                            <Grid item>
+                                <Button
+                                    color="secondary"
+                                    variant="text"
+                                    disableElevation
+                                >
+                                    Reject Order
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    color="primary"
+                                    variant="contained"
+                                    disableElevation
+                                >
+                                    Complete Order
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
+                ))}
         </Container>
     );
 };
