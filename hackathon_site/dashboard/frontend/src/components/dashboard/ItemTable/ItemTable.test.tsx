f2854ee0c23f7cf1d93da753864c925daa25f0cc
YuYing Liang <yuying.liang00@gmail.com>
2022-06-26 22:35:16 -0400

    fixed failing test

--- hackathon_site/dashboard/frontend/src/components/dashboard/ItemTable/ItemTable.test.tsx
+++ hackathon_site/dashboard/frontend/src/components/dashboard/ItemTable/ItemTable.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { fireEvent, makeStoreWithEntities, render } from "testing/utils";
 import {
-    ChipStatus,
     PendingTables,
     CheckedOutTables,
     ReturnedTable,
@@ -15,23 +14,6 @@ import {
     mockReturnedOrdersInTable,
 } from "testing/mockData";
 import { ReturnOrderInTable } from "api/types";
-
-describe("<ChipStatus />", () => {
-    test("Ready status", () => {
-        const { getByText } = render(<ChipStatus status="Ready for Pickup" />);
-        expect(getByText("Ready for pickup")).toBeInTheDocument();
-    });
-
-    test("Pending status", () => {
-        const { getByText } = render(<ChipStatus status="Submitted" />);
-        expect(getByText("In progress")).toBeInTheDocument();
-    });
-
-    test("Error status", () => {
-        const { getByText } = render(<ChipStatus status="Error" />);
-        expect(getByText("Visit the tech station")).toBeInTheDocument();
-    });
-});
 
 describe("<PendingTables />", () => {
     it("Shows pending items and status chip", () => {
