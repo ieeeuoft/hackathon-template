1ef1002f3a92ba303acb02b5877d73b664214b96
YuYing Liang <yuying.liang00@gmail.com>
2022-07-04 19:30:55 -0400

    added tests to simple order tables

--- hackathon_site/dashboard/frontend/src/components/teamDetail/SimpleOrderTables/SimpleOrderTables.test.tsx
+++ hackathon_site/dashboard/frontend/src/components/teamDetail/SimpleOrderTables/SimpleOrderTables.test.tsx
@@ -1,7 +1,11 @@
-import { fireEvent, render } from "testing/utils";
-import { mockPendingOrders } from "testing/mockData";
+import { fireEvent, render, within, act } from "testing/utils";
+import { mockPendingOrdersInTable, mockReturnedOrdersInTable } from "testing/mockData";
 import React from "react";
-import { SimplePendingOrderFulfillmentTable } from "./SimpleOrderTables";
+import {
+    AdminReturnedItemsTable,
+    SimplePendingOrderFulfillmentTable,
+} from "./SimpleOrderTables";
+import { waitFor } from "@testing-library/react";
 
 describe("<SimplePendingOrderFulfillmentTable />", () => {
     it("Shows tables by default and toggles visibility when button is clicked", async () => {
@@ -10,32 +14,115 @@ describe("<SimplePendingOrderFulfillmentTable />", () => {
         );
         const button = getByText(/hide all/i);
 
-        mockPendingOrders.map(({ id }) => {
-            expect(getByText(`Order #${id}`)).toBeInTheDocument();
+        mockPendingOrdersInTable.forEach(({ id, status }) => {
+            if (status !== "Cancelled")
+                expect(getByText(`Order #${id}`)).toBeInTheDocument();
         });
         fireEvent.click(button);
         expect(getByText(/show all/i)).toBeInTheDocument();
-        mockPendingOrders.map(({ id }) => {
+        mockPendingOrdersInTable.forEach(({ id }) => {
             expect(queryByText(`Order #${id}`)).toBeNull();
         });
     });
 
-    it("Does not show complete order button for Ready to Pickup orders", () => {});
+    it("Does not show complete order button for Ready to Pickup orders", () => {
+        const { getByTestId } = render(<SimplePendingOrderFulfillmentTable />);
+
+        mockPendingOrdersInTable.forEach(({ id, status }) => {
+            if (status !== "Cancelled") {
+                const { getByText, queryByText } = within(
+                    getByTestId(`admin-simple-pending-order-${id}`)
+                );
+                expect(getByText(`Order #${id}`)).toBeInTheDocument();
+                expect(getByText(/reject order/i)).toBeInTheDocument();
+                if (status === "Submitted")
+                    expect(getByText(/complete order/i)).toBeInTheDocument();
+                else expect(queryByText(/complete order/i)).not.toBeInTheDocument();
+            }
+        });
+    });
+
+    it("Disables complete order button when items are unchecked", () => {
+        const { getByTestId } = render(<SimplePendingOrderFulfillmentTable />);
+
+        mockPendingOrdersInTable
+            .filter(({ status }) => status === "Submitted")
+            .map(({ id }) => {
+                const { getByText } = within(
+                    getByTestId(`admin-simple-pending-order-${id}`)
+                );
+                expect(getByText(/complete order/i).parentNode).toBeDisabled();
+            });
+    });
+
+    it("Enables complete order button when items are all checked", async () => {
+        const { getByTestId } = render(<SimplePendingOrderFulfillmentTable />);
+
+        const order = mockPendingOrdersInTable.find(
+            ({ status }) => status === "Submitted"
+        );
+
+        if (order) {
+            const { getByDisplayValue, getByText } = within(
+                getByTestId(`admin-simple-pending-order-${order.id}`)
+            );
+            expect(getByText(/complete order/i).parentNode).toBeDisabled();
+            act(() => {
+                order.hardwareInTableRow.forEach(({ id }) => {
+                    const checkbox = getByDisplayValue(`hardware-${id}`);
+                    expect(checkbox).not.toBeChecked();
+                    fireEvent.click(checkbox);
+                });
+            });
+            await waitFor(() => {
+                expect(getByText(/complete order/i).parentNode).toBeEnabled();
+                expect(getByDisplayValue("checkAll")).toBeChecked();
+            });
+        }
+    });
+
+    test("Check All checkbox is unchecked when some/all rows are unchecked", async () => {
+        const { getByTestId } = render(<SimplePendingOrderFulfillmentTable />);
+
+        const order = mockPendingOrdersInTable.find(
+            ({ status }) => status === "Submitted"
+        );
+
+        if (order) {
+            const { getByDisplayValue } = within(
+                getByTestId(`admin-simple-pending-order-${order.id}`)
+            );
+            const checkAllCheckbox = getByDisplayValue("checkAll");
+            expect(checkAllCheckbox).not.toBeChecked();
+            await act(async () => {
+                fireEvent.click(checkAllCheckbox);
+                await waitFor(() => {
+                    order.hardwareInTableRow.forEach(({ id }) => {
+                        expect(getByDisplayValue(`hardware-${id}`)).toBeChecked();
+                    });
+                });
+                fireEvent.click(
+                    getByDisplayValue(`hardware-${order.hardwareInTableRow[0].id}`)
+                );
+                await waitFor(() => {
+                    expect(checkAllCheckbox).not.toBeChecked();
+                });
+            });
+        }
+    });
 });
 
 describe("<AdminReturnedItemsTable />", () => {
     it("Shows tables by default and toggles visibility when button is clicked", async () => {
-        const { getByText, queryByText } = render(
-            <SimplePendingOrderFulfillmentTable />
-        );
+        const { getByText, queryByText } = render(<AdminReturnedItemsTable />);
         const button = getByText(/hide all/i);
 
-        mockPendingOrders.map(({ id }) => {
+        mockReturnedOrdersInTable.map(({ id }) => {
             expect(getByText(`Order #${id}`)).toBeInTheDocument();
         });
         fireEvent.click(button);
         expect(getByText(/show all/i)).toBeInTheDocument();
-        mockPendingOrders.map(({ id }) => {
+        mockReturnedOrdersInTable.map(({ id }) => {
             expect(queryByText(`Order #${id}`)).toBeNull();
         });
     });
--- hackathon_site/dashboard/frontend/src/components/teamDetail/SimpleOrderTables/SimpleOrderTables.tsx
+++ hackathon_site/dashboard/frontend/src/components/teamDetail/SimpleOrderTables/SimpleOrderTables.tsx
@@ -1,5 +1,5 @@
 import { mockPendingOrdersInTable, mockReturnedOrdersInTable } from "testing/mockData";
-import React, { ChangeEvent, ChangeEventHandler, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import Container from "@material-ui/core/Container";
 import styles from "components/general/OrderTables/OrderTables.module.scss";
 import { Checkbox, FormGroup, Grid, TableCell, Tooltip } from "@material-ui/core";
@@ -9,64 +9,148 @@ import {
     GeneralPendingTable,
     GeneralReturnTable,
 } from "components/general/OrderTables/OrderTables";
-import { Field, FieldProps, Form, Formik } from "formik";
+import {
+    Field,
+    FieldProps,
+    Form,
+    Formik,
+    FormikValues,
+    useFormikContext,
+} from "formik";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { OrderInTable } from "api/types";
+
+interface SimpleOrderFormValues {
+    itemIdsChecked: string[];
+    checkAll: boolean;
+}
 
 const TableCheckbox = ({
     field,
     option,
-    recheckAll,
+    order,
     ...props
-}: FieldProps & { option: string; recheckAll: (e: ChangeEvent<{}>) => void }) => (
-    <FormGroup {...field} {...props}>
-        <FormControlLabel
-            name={field.name}
-            value={option}
-            control={<Checkbox color="primary" />}
-            label=""
-            checked={field.value?.includes(option)}
-            onChange={(e) => {
-                field.onChange(e);
-                recheckAll(e);
-            }}
-        />
-    </FormGroup>
-);
+}: FieldProps & { option: string; order: OrderInTable }) => {
+    const { values: formikValues, setFieldValue } =
+        useFormikContext<SimpleOrderFormValues>();
+
+    const recheckAllItems = (checked: boolean) => {
+        if (order.hardwareInTableRow.length - formikValues.itemIdsChecked.length <= 1) {
+            setFieldValue("checkAll", checked);
+        }
+    };
+
+    return (
+        <FormGroup {...field} {...props}>
+            <FormControlLabel
+                name={field.name}
+                value={option}
+                control={<Checkbox color="primary" />}
+                label=""
+                checked={field.value?.includes(option)}
+                onChange={(e, checked) => {
+                    field.onChange(e);
+                    recheckAllItems(checked);
+                }}
+            />
+        </FormGroup>
+    );
+};
 
 const TableCheckAll = ({
     field,
-    checkAll,
+    order,
     ...props
-}: FieldProps & { checkAll: (e: ChangeEvent<{}>) => void }) => (
-    <FormGroup {...field} {...props}>
-        <FormControlLabel
-            name={field.name}
-            value={field.value}
-            onChange={checkAll}
-            control={<Checkbox color="primary" />}
-            label=""
-            checked={field.value}
-        />
-    </FormGroup>
-);
+}: FieldProps & { order: OrderInTable }) => {
+    const { setFieldValue } = useFormikContext<SimpleOrderFormValues>();
 
-const CompleteOrderButton = ({ isDisabled }: { isDisabled: boolean }) => (
-    <Button
-        color="primary"
-        variant="contained"
-        disableElevation
-        type="submit"
-        disabled={isDisabled}
-    >
-        Complete Order
-    </Button>
-);
+    const checkAllItems = (checked: boolean) =>
+        setFieldValue(
+            "itemIdsChecked",
+            checked ? order.hardwareInTableRow.map((item) => `hardware-${item.id}`) : []
+        );
+
+    return (
+        <FormGroup {...field} {...props}>
+            <FormControlLabel
+                name={field.name}
+                value={field.name}
+                onChange={(e, checked) => {
+                    field.onChange(e);
+                    checkAllItems(checked);
+                }}
+                control={<Checkbox color="primary" />}
+                label=""
+                checked={field.value}
+            />
+        </FormGroup>
+    );
+};
+
+const getExtraColumn = (order: OrderInTable) => ({
+    extraColumn: {
+        header: (
+            <TableCell className={`${styles.width1} ${styles.noWrap}`} align="center">
+                <Field name="checkAll" component={TableCheckAll} order={order} />
+            </TableCell>
+        ),
+        body: (id: number) => (
+            <TableCell className={`${styles.width1} ${styles.noWrap}`} align="center">
+                <Field
+                    name="itemIdsChecked"
+                    component={TableCheckbox}
+                    option={`hardware-${id}`}
+                    order={order}
+                />
+            </TableCell>
+        ),
+    },
+});
+
+const CompleteOrderButton = ({ order }: { order: OrderInTable }) => {
+    const { values: formikValues } = useFormikContext<SimpleOrderFormValues>();
+
+    const disableCompletion =
+        formikValues.itemIdsChecked.length !== order.hardwareInTableRow.length;
+
+    const submitButton = (isDisabled: boolean) => (
+        <Button
+            color="primary"
+            variant="contained"
+            disableElevation
+            type="submit"
+            disabled={isDisabled}
+        >
+            Complete Order
+        </Button>
+    );
+
+    return (
+        <Grid item>
+            {disableCompletion ? (
+                <Tooltip
+                    title="You must check all items to fulfill the order"
+                    placement="top"
+                >
+                    <span>{submitButton(true)}</span>
+                </Tooltip>
+            ) : (
+                submitButton(false)
+            )}
+        </Grid>
+    );
+};
 
 export const SimplePendingOrderFulfillmentTable = () => {
     const orders = mockPendingOrdersInTable;
 
     const [isVisible, setVisibility] = useState(true);
     const toggleVisibility = () => setVisibility(!isVisible);
+
+    // TODO: update order status via patch request
+    const handleSubmit = (values: FormikValues) => {
+        console.log(values);
+    };
 
     return (
         <Container
@@ -93,115 +177,38 @@ export const SimplePendingOrderFulfillmentTable = () => {
                             itemIdsChecked: [],
                             checkAll: false,
                         }}
-                        onSubmit={(values) => console.log(values)}
+                        onSubmit={handleSubmit}
                     >
-                        {({ values, setFieldValue }) => {
-                            const disableCompletion =
-                                values.itemIdsChecked.length !==
-                                pendingOrder.hardwareInTableRow.length;
-                            const checkAllItems = (e: ChangeEvent<HTMLInputElement>) =>
-                                setFieldValue(
-                                    "itemIdsChecked",
-                                    e.target.checked
-                                        ? pendingOrder.hardwareInTableRow.map(
-                                              (item) => `hardware-${item.id}`
-                                          )
-                                        : []
-                                );
-                            const recheckAllItems = (
-                                e: ChangeEvent<HTMLInputElement>
-                            ) => {
-                                if (
-                                    pendingOrder.hardwareInTableRow.length -
-                                        values.itemIdsChecked.length <=
-                                    1
-                                ) {
-                                    setFieldValue("checkAll", e.target.checked);
-                                }
-                            };
-                            return (
-                                <Form
-                                    data-testid={`admin-pending-order-${pendingOrder.id}`}
-                                >
-                                    <GeneralPendingTable
-                                        {...{
-                                            pendingOrder,
-                                            ...(pendingOrder.status === "Submitted" && {
-                                                extraColumn: {
-                                                    header: (
-                                                        <TableCell
-                                                            className={`${styles.width1} ${styles.noWrap}`}
-                                                            align="center"
-                                                        >
-                                                            <Field
-                                                                name="checkAll"
-                                                                component={
-                                                                    TableCheckAll
-                                                                }
-                                                                checkAll={checkAllItems}
-                                                            />
-                                                        </TableCell>
-                                                    ),
-                                                    body: (id) => (
-                                                        <TableCell
-                                                            className={`${styles.width1} ${styles.noWrap}`}
-                                                            align="center"
-                                                        >
-                                                            <Field
-                                                                name="itemIdsChecked"
-                                                                component={
-                                                                    TableCheckbox
-                                                                }
-                                                                option={`hardware-${id}`}
-                                                                recheckAll={
-                                                                    recheckAllItems
-                                                                }
-                                                            />
-                                                        </TableCell>
-                                                    ),
-                                                },
-                                            }),
-                                        }}
-                                    />
-                                    <Grid
-                                        container
-                                        justifyContent="flex-end"
-                                        spacing={1}
-                                        style={{ marginTop: "10px" }}
+                        <Form
+                            data-testid={`admin-simple-pending-order-${pendingOrder.id}`}
+                        >
+                            <GeneralPendingTable
+                                {...{
+                                    pendingOrder,
+                                    ...(pendingOrder.status === "Submitted" &&
+                                        getExtraColumn(pendingOrder)),
+                                }}
+                            />
+                            <Grid
+                                container
+                                justifyContent="flex-end"
+                                spacing={1}
+                                style={{ marginTop: "10px" }}
+                            >
+                                <Grid item>
+                                    <Button
+                                        color="secondary"
+                                        variant="text"
+                                        disableElevation
                                     >
-                                        <Grid item>
-                                            <Button
-                                                color="secondary"
-                                                variant="text"
-                                                disableElevation
-                                            >
-                                                Reject Order
-                                            </Button>
-                                        </Grid>
-                                        {pendingOrder.status === "Submitted" && (
-                                            <Grid item>
-                                                {disableCompletion ? (
-                                                    <Tooltip
-                                                        title="You must check all items to fulfill the order"
-                                                        placement="top"
-                                                    >
-                                                        <span>
-                                                            <CompleteOrderButton
-                                                                isDisabled={true}
-                                                            />
-                                                        </span>
-                                                    </Tooltip>
-                                                ) : (
-                                                    <CompleteOrderButton
-                                                        isDisabled={false}
-                                                    />
-                                                )}
-                                            </Grid>
-                                        )}
-                                    </Grid>
-                                </Form>
-                            );
-                        }}
+                                        Reject Order
+                                    </Button>
+                                </Grid>
+                                {pendingOrder.status === "Submitted" && (
+                                    <CompleteOrderButton order={pendingOrder} />
+                                )}
+                            </Grid>
+                        </Form>
                     </Formik>
                 ))}
         </Container>
@@ -211,11 +218,15 @@ export const SimplePendingOrderFulfillmentTable = () => {
 export const AdminReturnedItemsTable = () => {
     const orders = mockReturnedOrdersInTable;
 
+    const [isVisible, setVisibility] = useState(true);
+    const toggleVisibility = () => setVisibility(!isVisible);
+
     return (
         <GeneralReturnTable
             {...{
                 orders,
-                isVisible: true,
+                isVisible,
+                toggleVisibility,
             }}
         />
     );
