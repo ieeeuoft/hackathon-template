82d4e62b61117405547808ba2270b33a1966f784
Karandeep Lubana <karandeep.lubana@utoronto.ca>
2022-07-20 10:02:59 -0400

    IEEE-204 wrap test cases with describe block

--- hackathon_site/dashboard/frontend/src/components/orders/OrderCard/OrderCard.test.tsx
+++ hackathon_site/dashboard/frontend/src/components/orders/OrderCard/OrderCard.test.tsx
@@ -3,31 +3,32 @@ import { render } from "testing/utils";
 import { mockPendingOrders } from "testing/mockData";
 import OrderCard from "./OrderCard";
 
-test("renders order card without crashing", () => {
-    const teamId = mockPendingOrders[1].team_id;
-    const orderQuantity = mockPendingOrders[1].items.length;
-    const timeOrdered = new Date(mockPendingOrders[1].created_at).toLocaleTimeString(
-        [],
-        { hour: "2-digit", minute: "2-digit" }
-    );
-    const id = mockPendingOrders[1].id;
-    const arr = [
-        { title: "Team", value: teamId },
-        { title: "Order Qty", value: orderQuantity },
-        { title: "Time ordered", value: timeOrdered },
-        { title: "ID", value: id },
-    ];
+describe("<OrderCard />", () => {
+    test("renders order card without crashing", () => {
+        const teamCode = mockPendingOrders[1].team_code;
+        const orderQuantity = mockPendingOrders[1].items.length;
+        const timeOrdered = new Date(
+            mockPendingOrders[1].created_at
+        ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+        const id = mockPendingOrders[1].id;
+        const orderDetails = [
+            { title: "Team", value: teamCode },
+            { title: "Order Qty", value: orderQuantity },
+            { title: "Time ordered", value: timeOrdered },
+            { title: "ID", value: id },
+        ];
 
-    const { getByText } = render(
-        <OrderCard
-            teamId={teamId}
-            orderQuantity={orderQuantity}
-            timeOrdered={timeOrdered}
-            id={id}
-        />
-    );
-    arr.forEach((item) => {
-        expect(getByText(item.title)).toBeInTheDocument();
-        expect(getByText(item.value.toString())).toBeInTheDocument();
+        const { getByText } = render(
+            <OrderCard
+                teamCode={teamCode}
+                orderQuantity={orderQuantity}
+                timeOrdered={timeOrdered}
+                id={id}
+            />
+        );
+        orderDetails.forEach((item) => {
+            expect(getByText(item.title)).toBeInTheDocument();
+            expect(getByText(item.value.toString())).toBeInTheDocument();
+        });
     });
 });
