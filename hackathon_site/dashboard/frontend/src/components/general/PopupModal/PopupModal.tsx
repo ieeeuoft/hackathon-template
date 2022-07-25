c365497b6de00c75d32a018b41c8e4d8d208f4f7
ericji1326 <ericji1326@gmail.com>
2022-07-24 23:46:50 -0400

    new changes to fix modal styling and unit tests

--- hackathon_site/dashboard/frontend/src/components/general/PopupModal/PopupModal.tsx
+++ hackathon_site/dashboard/frontend/src/components/general/PopupModal/PopupModal.tsx
@@ -3,6 +3,7 @@ import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
 import styles from "components/general/PopupModal/PopupModal.module.scss";
 import { Box } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 
 interface PopupModalProps {
     title?: string;
@@ -10,7 +11,7 @@ interface PopupModalProps {
     isVisible: boolean;
     submitText?: string;
     cancelText?: string;
-    submitHandler(): any;
+    submitHandler?(): any;
     cancelHandler?(): any;
 }
 
@@ -24,15 +25,22 @@ const PopupModal = ({
     cancelHandler,
 }: PopupModalProps) => {
     return (
-        <Modal open={isVisible}>
-            <Box className={styles.modalSize}>
-                <h1 className={styles.title}>{title}</h1>
-                <p className={styles.description}>{description}</p>
-                <div className={styles.buttons}>
+        <Modal open={isVisible} className={styles.modal}>
+            <Grid
+                className={styles.modalGrid}
+                container
+                direction={"column"}
+                spacing={2}
+            >
+                <Grid item>{title && <h1 className={styles.title}>{title}</h1>}</Grid>
+                <Grid item>
+                    <p className={styles.description}>{description}</p>
+                </Grid>
+                <Grid item container justifyContent={"flex-end"}>
                     <Button onClick={cancelHandler}>{cancelText}</Button>
                     <Button onClick={submitHandler}>{submitText}</Button>
-                </div>
-            </Box>
+                </Grid>
+            </Grid>
         </Modal>
     );
 };
