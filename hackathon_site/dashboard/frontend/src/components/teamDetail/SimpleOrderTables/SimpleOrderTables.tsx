022fc9530d06e7771a21990c74bf240566af0e64
EthanHugh <84813627+EthanHugh@users.noreply.github.com>
2022-09-23 17:31:48 -0400

    Ieee 218 update order status thunk (#434)
    * Created a thunk for updating order status
    
    * changed the TeamPendingOrderTable to use actual data instead of mock data
    
    * The buttons for the TeamPendingOrderTable now correctly change the order status, but it does not do any UI changes, such as disable buttons, loading icon and refresh
    
    * Wrote down tests and finished the UI elements when clicking on the Complete Order or Reject Order buttons. There is a weird bug when refreshing the page though
    
    * changed the selector for admin pending orders to be called in TeamDetail instead, and made TeamPendingOrderTable take an argument called orders that should contain all orders to display
    
    * removed unused imports
    
    * Since hardware selectors and mockHardware are different data types, I made hardware a field that takes either an array or hardware or null, and if null is passed then it uses the selector, otherwise it uses a Hardware array that should be passed into TeamPendingOrderTable
    
    * Completed the changes mentioned in the PR and added the updateOrder function to SimplePendingOrderFulfillmentTable
    
    * made it so that TeamPendingOrder tests pass by mocking the store
    
    * Implemented the changes requested in the PR
    
    Co-authored-by: Mustafa <mus2003.abdul@gmail.com>
    Co-authored-by: YuYing_Liang <yuying.liang00@gmail.com>

--- hackathon_site/dashboard/frontend/src/components/teamDetail/SimpleOrderTables/SimpleOrderTables.tsx
+++ hackathon_site/dashboard/frontend/src/components/teamDetail/SimpleOrderTables/SimpleOrderTables.tsx
@@ -17,16 +17,23 @@ import {
     useFormikContext,
 } from "formik";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { OrderInTable } from "api/types";
-import { useSelector } from "react-redux";
+import { OrderInTable, OrderStatus } from "api/types";
+import { useDispatch, useSelector } from "react-redux";
 import {
     pendingOrdersSelector,
     returnedOrdersSelector,
+    UpdateOrderAttributes,
+    updateOrderStatus,
 } from "slices/order/teamOrderSlice";
 
 interface SimpleOrderFormValues {
     itemIdsChecked: string[];
     checkAll: boolean;
+}
+
+interface UpdateOrderValues {
+    id: number;
+    status: OrderStatus;
 }
 
 const TableCheckbox = ({
@@ -146,14 +153,18 @@ const CompleteOrderButton = ({ order }: { order: OrderInTable }) => {
 };
 
 export const SimplePendingOrderFulfillmentTable = () => {
+    const dispatch = useDispatch();
     const orders = useSelector(pendingOrdersSelector);
 
     const [isVisible, setVisibility] = useState(true);
     const toggleVisibility = () => setVisibility(!isVisible);
 
-    // TODO: update order status via patch request
-    const handleSubmit = (values: FormikValues) => {
-        console.log(values);
+    const updateOrder = (orderId: number, status: OrderStatus) => {
+        const updateOrderData: UpdateOrderAttributes = {
+            id: orderId,
+            status: status,
+        };
+        dispatch(updateOrderStatus(updateOrderData));
     };
 
     return (
@@ -181,7 +192,9 @@ export const SimplePendingOrderFulfillmentTable = () => {
                             itemIdsChecked: [],
                             checkAll: false,
                         }}
-                        onSubmit={handleSubmit}
+                        onSubmit={(values) => {
+                            updateOrder(pendingOrder.id, "Ready for Pickup");
+                        }}
                     >
                         <Form
                             data-testid={`admin-simple-pending-order-${pendingOrder.id}`}
@@ -199,15 +212,23 @@ export const SimplePendingOrderFulfillmentTable = () => {
                                 spacing={1}
                                 style={{ marginTop: "10px" }}
                             >
-                                <Grid item>
-                                    <Button
-                                        color="secondary"
-                                        variant="text"
-                                        disableElevation
-                                    >
-                                        Reject Order
-                                    </Button>
-                                </Grid>
+                                {pendingOrder.status === "Submitted" && (
+                                    <Grid item>
+                                        <Button
+                                            color="secondary"
+                                            variant="text"
+                                            disableElevation
+                                            onClick={() =>
+                                                updateOrder(
+                                                    pendingOrder.id,
+                                                    "Cancelled"
+                                                )
+                                            }
+                                        >
+                                            Reject Order
+                                        </Button>
+                                    </Grid>
+                                )}
                                 {pendingOrder.status === "Submitted" && (
                                     <CompleteOrderButton order={pendingOrder} />
                                 )}
@@ -222,6 +243,12 @@ export const SimplePendingOrderFulfillmentTable = () => {
                                                     color="secondary"
                                                     variant="contained"
                                                     disableElevation
+                                                    onClick={() =>
+                                                        updateOrder(
+                                                            pendingOrder.id,
+                                                            "Picked Up"
+                                                        )
+                                                    }
                                                 >
                                                     Picked Up
                                                 </Button>
