38c1b963c8092bc20e9c0242e3bea450ca3c3041
Daniel Qiu <qiudaniel03@gmail.com>
2023-01-05 15:55:55 -0500

    wrote selector to obtain the number of orders for the admin dashboard and updated the result count

--- hackathon_site/dashboard/frontend/src/components/orders/OrdersCount/OrdersCount.tsx
+++ hackathon_site/dashboard/frontend/src/components/orders/OrdersCount/OrdersCount.tsx
@@ -3,11 +3,14 @@ import { IconButton, Typography } from "@material-ui/core";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import styles from "pages/Orders/Orders.module.scss";
 import { FormikValues } from "formik";
+import { adminOrderTotalSelector } from "../../../slices/order/adminOrderSlice";
+import { useSelector } from "react-redux";
 
 const OrdersCount = ({ refreshOrders }: FormikValues) => {
+    const orderQuantity = useSelector(adminOrderTotalSelector);
     return (
         <div className={styles.ordersBodyToolbarRefresh}>
-            <Typography variant="body2">2 results</Typography>
+            <Typography variant="body2">{orderQuantity} results</Typography>
             <IconButton
                 color="primary"
                 aria-label="Refresh"
