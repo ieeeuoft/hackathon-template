a8f57e733e3719dd454615312e1864d2526631ce
root <root@DESKTOP-JK8HLM9.localdomain>
2023-01-07 18:06:05 -0500

    added product overview test to TeamDetail.test.tsx

--- hackathon_site/dashboard/frontend/src/pages/TeamDetail/TeamDetail.test.tsx
+++ hackathon_site/dashboard/frontend/src/pages/TeamDetail/TeamDetail.test.tsx
@@ -6,12 +6,23 @@ import {
     when,
     waitFor,
     makeMockApiResponse,
+    promiseResolveWithDelay,
 } from "testing/utils";
-import { mockOrders, mockTeam, mockTeamMultiple } from "testing/mockData";
+import {
+    cardItems,
+    mockCategories,
+    mockCheckedOutOrders,
+    mockHardware,
+    mockOrders,
+    mockTeam,
+    mockTeamMultiple,
+} from "testing/mockData";
 import TeamDetail, { PageParams } from "pages/TeamDetail/TeamDetail";
 import { RouteComponentProps } from "react-router-dom";
-import { Order } from "api/types";
-import { get } from "api/api";
+import { Hardware, Order } from "api/types";
+import { get, patch } from "api/api";
+import Dashboard from "../Dashboard/Dashboard";
+import { fireEvent, within } from "../../testing/utils";
 
 jest.mock("api/api", () => ({
     ...jest.requireActual("api/api"),
@@ -29,6 +40,26 @@ const teamDetailProps = {
         },
     },
 } as RouteComponentProps<PageParams>;
+
+const mockedPatch = patch as jest.MockedFunction<typeof patch>;
+
+const hardwareUri = "/api/hardware/hardware/";
+const categoriesUri = "/api/hardware/categories/";
+const teamUri = "/api/event/teams/team/";
+const ordersUri = "/api/event/teams/team/orders/";
+
+const mockOrderAPI = (orders?: Order[]) =>
+    when(mockedGet)
+        .calledWith(ordersUri)
+        .mockResolvedValue(makeMockApiListResponse<Order>(orders ?? mockOrders));
+const mockTeamAPI = (withDelay?: boolean) => {
+    const teamAPIResponse = makeMockApiResponse(mockTeam);
+    when(mockedGet)
+        .calledWith(teamUri)
+        .mockResolvedValue(
+            withDelay ? promiseResolveWithDelay(teamAPIResponse, 500) : teamAPIResponse
+        );
+};
 
 describe("<TeamDetail />", () => {
     test("renders loading component and then data without crashing", async () => {
@@ -83,5 +114,77 @@ describe("<TeamDetail />", () => {
                 screen.getByText("Could not find team code: Error 404")
             ).toBeInTheDocument();
         });
+    });
+
+    test("Opens Product Overview with the correct hardware information", async () => {
+        const hardwareDetailUri = "/api/hardware/hardware/1/";
+        const newHardwareData: Hardware = {
+            id: mockCheckedOutOrders[0].items[0].hardware_id,
+            name: "Random hardware",
+            model_number: "90",
+            manufacturer: "Tesla",
+            datasheet: "",
+            quantity_available: 5,
+            max_per_team: 6,
+            picture: "https://example.com/datasheet",
+            categories: [2],
+            quantity_remaining: 10,
+            notes: "notes on temp",
+        };
+
+        const hardwareApiResponse = makeMockApiListResponse(mockHardware);
+        const categoryApiResponse = makeMockApiListResponse(mockCategories);
+        const hardwareDetailApiResponse = makeMockApiResponse(newHardwareData);
+        const hardware_ids = [1, 2, 3, 4, 10];
+
+        mockOrderAPI();
+        when(mockedGet)
+            .calledWith(hardwareUri, { hardware_ids })
+            .mockResolvedValue(hardwareApiResponse);
+        when(mockedGet)
+            .calledWith(categoriesUri, {})
+            .mockResolvedValue(categoryApiResponse);
+        when(mockedGet)
+            .calledWith(hardwareDetailUri)
+            .mockResolvedValue(hardwareDetailApiResponse);
+
+        const { getByTestId, getByText } = render(<TeamDetail {...teamDetailProps} />);
+
+        const category = mockCategories.find(
+            ({ id }) => id === newHardwareData?.categories[0]
+        );
+
+        if (category) {
+            await waitFor(() => {
+                // expect(get).toHaveBeenNthCalledWith(1, teamUri);
+                // expect(get).toHaveBeenNthCalledWith(2, categoriesUri, {});
+                // expect(get).toHaveBeenNthCalledWith(3, ordersUri);
+                // expect(get).toHaveBeenNthCalledWith(4, hardwareUri, { hardware_ids });
+            });
+            await waitFor(() => {
+                const infoButton = within(
+                    getByTestId(
+                        `table-${newHardwareData.id}-${mockCheckedOutOrders[0].id}`
+                    )
+                ).getByTestId("info-button");
+                fireEvent.click(infoButton);
+            });
+            await waitFor(() => {
+                // expect(get).toHaveBeenNthCalledWith(5, hardwareDetailUri);
+                expect(getByText("Product Overview")).toBeVisible();
+                expect(
+                    getByText(`- Max ${newHardwareData.max_per_team} of this item`)
+                ).toBeInTheDocument();
+                expect(
+                    getByText(
+                        `- Max ${category.max_per_team} of items under category ${category.name}`
+                    )
+                ).toBeInTheDocument();
+                expect(getByText(newHardwareData.model_number)).toBeInTheDocument();
+                expect(getByText(newHardwareData.manufacturer)).toBeInTheDocument();
+                if (newHardwareData.notes)
+                    expect(getByText(newHardwareData.notes)).toBeInTheDocument();
+            });
+        }
     });
 });
