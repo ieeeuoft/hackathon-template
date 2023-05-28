f05288ca2496e89038e9da0ac6eaea915796d218
Natalie <natapoke2@gmail.com>
2023-05-28 17:04:22 -0400

    reverting changes

--- hackathon_site/dashboard/frontend/src/components/orders/OrderCard/OrderCheckedOut.tsx
+++ hackathon_site/dashboard/frontend/src/components/orders/OrderCard/OrderCheckedOut.tsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./OrderCard.module.scss";
 import { Typography, Container, Card } from "@material-ui/core";
-import { Formik, FormikValues } from "formik";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Button from "@material-ui/core/Button";
-import stylesButton from "components/sharedStyles/Filter.module.scss";
 
 interface OrderCheckedOutProps {
     team: string;
@@ -35,91 +30,20 @@ const OrderCheckedOut = ({
         { title: "ID", value: id },
     ];
 
-    // controls the state of the button
-    const [pickedUp, setPickedUp] = useState(false);
-
-    const alertEvent = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        event.preventDefault();
-        if (event.target === event.currentTarget) {
-            alert("The order you clicked is: #" + id);
-        }
-    };
-
     return (
-        <Card onClick={alertEvent}>
+        <Card>
             <Container className={styles.container} data-testid={`order-item-${id}`}>
-                {orderDetails.map((item, idx) => {
-                    return (
-                        <Container
-                            className={styles.contentContainer}
-                            key={idx}
-                            style={{ alignItems: "center" }}
-                        >
-                            <Typography variant="body2" className={styles.title}>
-                                {item.title}
-                            </Typography>
-                            {item.title === "Received IDs" ? (
-                                <Checkbox
-                                    color="primary"
-                                    checked={
-                                        typeof item.value === "boolean"
-                                            ? item.value
-                                            : false
-                                    }
-                                    disabled={pickedUp}
-                                />
-                            ) : (
-                                <Typography
-                                    variant="body2"
-                                    style={{ textAlign: "end" }}
-                                >
-                                    {item.value}
-                                </Typography>
-                            )}
-                        </Container>
-                    );
-                })}
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        padding: "4px 12px",
-                    }}
-                >
-                    <Button
-                        type="submit"
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            console.log("click picked up");
-                            setPickedUp(true);
-                        }}
-                        color="primary"
-                        variant="contained"
-                        fullWidth={true}
-                        className={stylesButton.filterBtnsApply}
-                        disabled={pickedUp}
-                        disableElevation
-                        data-testid="apply-button"
-                        style={pickedUp ? { pointerEvents: "none" } : {}}
-                    >
-                        Picked Up
-                    </Button>
-                    <Button
-                        type="submit"
-                        color="primary"
-                        data-testid="clear-button"
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            console.log("click view");
-                        }}
-                    >
-                        View
-                    </Button>
-                </div>
+                {orderDetails.map((item, idx) => (
+                    <Container className={styles.contentContainer} key={idx}>
+                        <Typography variant="body2" className={styles.title}>
+                            {item.title}
+                        </Typography>
+                        <Typography variant="body2" style={{ textAlign: "end" }}>
+                            {item.value}
+                        </Typography>
+                    </Container>
+                ))}
             </Container>
-            {/* </Formik> */}
         </Card>
     );
 };
