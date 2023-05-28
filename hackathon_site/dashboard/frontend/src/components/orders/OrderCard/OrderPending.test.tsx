9bcb95853a26452212eecc627d77d236e73e6d64
Natalie <natapoke2@gmail.com>
2023-05-28 17:12:02 -0400

    testing file fix

--- hackathon_site/dashboard/frontend/src/components/orders/OrderCard/OrderPending.test.tsx
+++ hackathon_site/dashboard/frontend/src/components/orders/OrderCard/OrderPending.test.tsx
@@ -3,7 +3,7 @@ import { render } from "testing/utils";
 import { mockPendingOrders } from "testing/mockData";
 import OrderPending from "./OrderPending";
 
-describe("<OrderCard />", () => {
+describe("<OrderPending />", () => {
     test("renders order pending card without crashing", () => {
         const teamCode = mockPendingOrders[1].team_code;
         const orderQuantity = mockPendingOrders[1].items.length;
@@ -12,7 +12,7 @@ describe("<OrderCard />", () => {
             minute: "2-digit",
         });
         const id = mockPendingOrders[1].id;
-        const receivedIDs = true
+        const receivedIDs = true;
         const date = new Date(time);
         const month = date.toLocaleString("default", { month: "short" });
         const day = date.getDate();
@@ -28,11 +28,11 @@ describe("<OrderCard />", () => {
 
         const { getByText } = render(
             <OrderPending
-            team={teamCode}
-            orderQuantity={orderQuantity}
-            timeOrdered={time}
-            receivedIDs={receivedIDs}
-            id={id}
+                team={teamCode}
+                orderQuantity={orderQuantity}
+                timeOrdered={time}
+                receivedIDs={receivedIDs}
+                id={id}
             />
         );
         orderDetails.forEach((item) => {
