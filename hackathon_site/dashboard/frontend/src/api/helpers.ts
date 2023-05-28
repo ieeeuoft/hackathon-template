1b4c1e5439771aabf4e7be14a6d2ee7fb444bb15
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2023-05-27 23:36:10 -0400

    more changes to helper.ts

--- hackathon_site/dashboard/frontend/src/api/helpers.ts
+++ hackathon_site/dashboard/frontend/src/api/helpers.ts
@@ -6,18 +6,18 @@ import {
     ReturnOrderInTable,
 } from "api/types";
 
-function formatDateTime(dateTimeString: string): string {
-    const dateTime = new Date(dateTimeString);
-    const options: Intl.DateTimeFormatOptions = {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-    };
-    return dateTime.toLocaleString("en-US", options);
-}
+// function formatDateTime(dateTimeString: string): string {
+//     const dateTime = new Date(dateTimeString);
+//     const options: Intl.DateTimeFormatOptions = {
+//         month: "long",
+//         day: "numeric",
+//         year: "numeric",
+//         hour: "numeric",
+//         minute: "numeric",
+//         hour12: true,
+//     };
+//     return dateTime.toLocaleString("en-US", options);
+// }
 
 export const teamOrderListSerialization = (
     orders: Order[]
