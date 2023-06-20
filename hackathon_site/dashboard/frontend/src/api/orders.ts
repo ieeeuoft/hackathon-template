7698d6f9f6aac93741638308e28c34048fc9bf91
Natalie <natapoke2@gmail.com>
2023-06-19 22:57:18 -0400

    changing imports

--- hackathon_site/dashboard/frontend/src/api/orders.ts
+++ hackathon_site/dashboard/frontend/src/api/orders.ts
@@ -1,12 +1,12 @@
-import SubmittedIcon from "../assets/images/icons/statusIcons/unfulfilled-status.svg";
-import ReadyForPickupIcon from "../assets/images/icons/statusIcons/readyforpickup-status.svg";
-import PickedUpIcon from "../assets/images/icons/statusIcons/checkout-status.svg";
-import CancelledIcon from "../assets/images/icons/statusIcons/cancelled-status.svg";
-import ReturnedIcon from "../assets/images/icons/statusIcons/checkout-status.svg";
-import PendingIcon from "../assets/images/icons/statusIcons/pending-status.svg";
-import InProgressIcon from "../assets/images/icons/statusIcons/inprogress-status.svg";
+import SubmittedIcon from "assets/images/icons/statusIcons/unfulfilled-status.svg";
+import ReadyForPickupIcon from "assets/images/icons/statusIcons/readyforpickup-status.svg";
+import PickedUpIcon from "assets/images/icons/statusIcons/checkout-status.svg";
+import CancelledIcon from "assets/images/icons/statusIcons/cancelled-status.svg";
+import ReturnedIcon from "assets/images/icons/statusIcons/checkout-status.svg";
+import PendingIcon from "assets/images/icons/statusIcons/pending-status.svg";
+import InProgressIcon from "assets/images/icons/statusIcons/inprogress-status.svg";
 
-import styles from "../components/orders/OrdersTable/OrdersTable.module.scss";
+import styles from "components/orders/OrdersTable/OrdersTable.module.scss";
 
 export const statusIconMap: { [key: string]: string } = {
     Submitted: SubmittedIcon,
