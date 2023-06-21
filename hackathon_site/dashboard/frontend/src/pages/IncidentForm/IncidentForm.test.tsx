4e0baf40c5854b52df4d8f3dec550fd2a5eb2125
Natalie <natapoke2@gmail.com>
2023-06-20 23:00:42 -0400

    fix type error

--- hackathon_site/dashboard/frontend/src/pages/IncidentForm/IncidentForm.test.tsx
+++ hackathon_site/dashboard/frontend/src/pages/IncidentForm/IncidentForm.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { fireEvent, render, waitFor } from "testing/utils";
+import { fireEvent, render, screen, waitFor } from "testing/utils";
 import IncidentForm from "./IncidentForm";
 import { INCIDENT_ERROR_MSG } from "./IncidentForm";
 
-describe("Incident Form", () => {
+describe("<Incident Form />", () => {
     test("Renders without crashing", () => {
-        const { getByText } = render(<IncidentForm />);
-        expect(getByText("Item Incident Form")).toBeInTheDocument();
+        render(<IncidentForm />);
+        expect(screen.getByText("Item Incident Form")).toBeInTheDocument();
     });
 });
