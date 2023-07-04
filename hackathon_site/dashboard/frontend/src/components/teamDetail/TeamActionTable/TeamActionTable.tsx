ce3974cc48a0deb99bce7dc428e0c0b37f48610f
ishika1202 <mittalishikaiit@gmail.com>
2023-07-04 18:00:42 -0400

    Implement extra warning delete feature

--- hackathon_site/dashboard/frontend/src/components/teamDetail/TeamActionTable/TeamActionTable.tsx
+++ hackathon_site/dashboard/frontend/src/components/teamDetail/TeamActionTable/TeamActionTable.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import {
     Grid,
     Paper,
@@ -8,7 +10,6 @@ import {
 } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
-import React, { useState } from "react";
 import NotificationImportantIcon from "@material-ui/icons/NotificationImportant";
 import CallSplitIcon from "@material-ui/icons/CallSplit";
 import MergeTypeIcon from "@material-ui/icons/MergeType";
@@ -16,6 +17,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import PopupModal from "components/general/PopupModal/PopupModal";
 import { useDispatch } from "react-redux";
 import { deleteTeamThunk } from "slices/event/teamAdminSlice";
+import { teamDetailAdapterSelector } from "slices/event/teamDetailSlice";
 
 interface TeamActionTableProps {
     teamCode: string;
@@ -23,6 +25,13 @@ interface TeamActionTableProps {
 
 const TeamActionTable = ({ teamCode }: TeamActionTableProps) => {
     const [visible, setVisible] = useState(false);
+    const [warningVisible, setWarningVisible] = useState(false);
+    const profiles = useSelector(teamDetailAdapterSelector.selectAll);
+
+    const hasMultipleTeamMembers = () => {
+        return profiles.length > 1;
+    };
+
     const actionButtons = [
         {
             icon: <NotificationImportantIcon />,
@@ -47,17 +56,30 @@ const TeamActionTable = ({ teamCode }: TeamActionTableProps) => {
             },
         },
     ];
-    const dispatch = useDispatch();
 
+    const dispatch = useDispatch();
     const history = useHistory();
 
     const deleteTeam = async () => {
         setVisible(false);
+        if (hasMultipleTeamMembers()) {
+            setWarningVisible(true);
+        } else {
+            try {
+                await dispatch(deleteTeamThunk(teamCode));
+                history.push("/teams");
+            } catch (error) {
+                console.error("Error deleting team:", error);
+            }
+        }
+    };
+
+    const handleWarningSubmit = async () => {
+        setWarningVisible(false);
         try {
             await dispatch(deleteTeamThunk(teamCode));
             history.push("/teams");
         } catch (error) {
-            // Handle any errors that occur during deletion
             console.error("Error deleting team:", error);
         }
     };
@@ -70,11 +92,7 @@ const TeamActionTable = ({ teamCode }: TeamActionTableProps) => {
 
             <List
                 component={Paper}
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    flex: 1,
-                }}
+                style={{ display: "flex", flexDirection: "column", flex: 1 }}
             >
                 {actionButtons.map((action, index) => (
                     <ListItem
@@ -82,15 +100,14 @@ const TeamActionTable = ({ teamCode }: TeamActionTableProps) => {
                         button
                         onClick={action.onClick}
                         disabled={action.onClick === undefined}
-                        style={{
-                            flex: 1,
-                        }}
+                        style={{ flex: 1 }}
                     >
                         <ListItemIcon>{action.icon}</ListItemIcon>
                         <ListItemText primary={action.text} />
                     </ListItem>
                 ))}
             </List>
+
             <PopupModal
                 title="Confirmation"
                 description={`Are you sure you want to delete this team (${teamCode})?`}
@@ -99,6 +116,16 @@ const TeamActionTable = ({ teamCode }: TeamActionTableProps) => {
                 cancelText="No"
                 submitHandler={deleteTeam}
                 cancelHandler={() => setVisible(false)}
+            />
+
+            <PopupModal
+                title="Warning"
+                description="This team has multiple members. Deleting it will also remove all team members. Are you sure you want to continue?"
+                isVisible={warningVisible}
+                submitText="Yes, Delete"
+                cancelText="Cancel"
+                submitHandler={handleWarningSubmit}
+                cancelHandler={() => setWarningVisible(false)}
             />
         </Grid>
     );
