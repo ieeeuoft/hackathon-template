6a22623970d86e8bdc0a4dc58e74caba868940f1
Carmen Chau <80921817+carmen-chau@users.noreply.github.com>
2023-09-19 22:04:53 -0400

    Removed useState and setState import

--- hackathon_site/dashboard/frontend/src/pages/Reports/Reports.js
+++ hackathon_site/dashboard/frontend/src/pages/Reports/Reports.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./Reports.module.scss";
 import Header from "components/general/Header/Header";
 import Typography from "@material-ui/core/Typography";
