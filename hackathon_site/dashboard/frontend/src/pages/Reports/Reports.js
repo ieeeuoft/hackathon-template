401a478a5390c11864042bcf59e38f146e7af9a8
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2023-10-22 18:14:03 -0400

    hopefully resolved test issues?

--- hackathon_site/dashboard/frontend/src/pages/Reports/Reports.js
+++ hackathon_site/dashboard/frontend/src/pages/Reports/Reports.js
@@ -3,7 +3,7 @@ import styles from "./Reports.module.scss";
 import Header from "components/general/Header/Header";
 import Typography from "@material-ui/core/Typography";
 
-// Import Grid for formatting
+// Import Grid for formatting information
 import { Grid } from "@material-ui/core";
 
 // Imports for the Table MUI Component
