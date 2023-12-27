085ef3091ad321f618b1804d45c63a165bdf58f7
Natalie Chan <86009011+natapokie@users.noreply.github.com>
2023-12-26 20:31:37 -0500

    Ieee 269 create table for orders page (#500)
    * orders table init
    
    * adding status icon
    
    * adding event handler for row double click & files
    
    * adding more statuses
    
    * adding unit tests
    
    * reverting yarn lock, seemed to have issues with testing
    
    * changing node version to 16 for mui data grid
    
    * cleaning up icon code
    
    * updating github actions
    
    * trying again
    
    * one more time...
    
    * trying this...
    
    * Update main.yml
    
    * adding styles to module.scss
    
    * making team id visible
    
    * changing imports
    
    * adding dependencies
    
    * fixing some code & commenting all tests for order cards
    
    * removing mockdata
    
    * commenting out unused vars
    
    * cleaning up code
    
    * adding more tests
    
    * adding handle double row click event
    
    * adding test to check for proper hanlding of double row click
    
    * using type any for valueGetter function
    
    * remove unused vars
    
    * removing comments
    
    * adding style to make the grid white
    
    ---------
    
    Co-authored-by: Mustafa <mus2003.abdul@gmail.com>

--- hackathon_site/dashboard/frontend/src/pages/Orders/Orders.test.tsx
+++ hackathon_site/dashboard/frontend/src/pages/Orders/Orders.test.tsx
@@ -20,39 +20,4 @@ describe("Orders Page", () => {
         expect(getByText("Orders")).toBeInTheDocument();
         expect(getByTestId("ordersCountDivider")).toBeInTheDocument();
     });
-
-    test("Display correct order cards", () => {
-        const { getByTestId } = render(<Orders />, { store });
-        mockPendingOrders.forEach((order) => {
-            const orderStatus = order.status;
-
-            if (orderStatus === "Submitted" || orderStatus === "Ready for Pickup") {
-                const orderItem = getByTestId(`order-item-${order.id}`);
-                const orderDetails = within(orderItem);
-                const date = new Date(order.created_at);
-                const month = date.toLocaleString("default", { month: "short" });
-                const day = date.getDate();
-                const hoursAndMinutes = date.getHours() + ":" + date.getMinutes();
-
-                expect(orderDetails.getByText(order.id)).toBeInTheDocument();
-                expect(orderDetails.getByText(order.items.length)).toBeInTheDocument();
-                expect(orderDetails.getByText(order.team_code)).toBeInTheDocument();
-                expect(
-                    orderDetails.getByText(`${month} ${day}, ${hoursAndMinutes}`)
-                ).toBeInTheDocument();
-            }
-            // TODO: Add tests for Checkout Orders in the future
-            // else {
-            //     expect(
-            //         orderDetails.getByText("Order Card Component")
-            //     ).toBeInTheDocument();
-            // }
-        });
-    });
-    test("Display correct amount of order results", () => {
-        const { getByText } = render(<Orders />, { store });
-        const orderCount = mockPendingOrders.length;
-        const resultText = getByText(`${orderCount} results`);
-        expect(resultText).toBeInTheDocument();
-    });
 });
